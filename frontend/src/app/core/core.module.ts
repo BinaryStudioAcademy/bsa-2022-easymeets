9f25f629e8dc756fc240be942bdaf3560f04bb7d
YuraRiabov <uraryabov@gmail.com>
2022-08-07 15:04:01 +0300

    Firebase initialization moved to app.module.ts to allow usage from other modules

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,15 +1,12 @@
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { environment } from '@env/environment';
 import { SharedModule } from '@shared/shared.module';
 
 import { BaseComponent } from './base/base.component';
 import { ErrorInterceptor } from './interceptors/error.interceptor';
 
 @NgModule({
-    imports: [HttpClientModule, SharedModule, AngularFireModule.initializeApp(environment.firebase), AngularFireAuthModule],
+    imports: [HttpClientModule, SharedModule],
     providers: [{ provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }],
     declarations: [BaseComponent],
 })
