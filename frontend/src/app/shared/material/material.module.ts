aa1896023f7b885a0aa088d11cb8f32fad74e9b0
Olena Kozak <olena.s.kozak@gmail.com>
2022-08-08 16:28:45 +0300

    Add base markup to slot component Remove custom theming

--- frontend/src/app/shared/material/material.module.ts
+++ frontend/src/app/shared/material/material.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
     declarations: [],
-    imports: [CommonModule, FormsModule, ReactiveFormsModule, MatCardModule, MatButtonModule],
-    exports: [FormsModule, ReactiveFormsModule, CommonModule, MatCardModule, MatButtonModule],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, MatCardModule, MatButtonModule, MatSlideToggleModule],
+    exports: [FormsModule, ReactiveFormsModule, CommonModule, MatCardModule, MatButtonModule, MatSlideToggleModule],
 })
 export class MaterialModule {}
