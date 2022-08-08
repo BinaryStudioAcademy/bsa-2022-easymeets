318e84b86417fcaf27f6f9f8c40b9805ba588347
Olena Kozak <olena.s.kozak@gmail.com>
2022-08-08 18:22:53 +0300

    Fix bug with grid layout

--- frontend/src/app/shared/material/material.module.ts
+++ frontend/src/app/shared/material/material.module.ts
@@ -3,11 +3,28 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
     declarations: [],
-    imports: [CommonModule, FormsModule, ReactiveFormsModule, MatCardModule, MatButtonModule, MatSlideToggleModule],
-    exports: [FormsModule, ReactiveFormsModule, CommonModule, MatCardModule, MatButtonModule, MatSlideToggleModule],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatCardModule,
+        MatButtonModule,
+        MatSlideToggleModule,
+        MatListModule,
+    ],
+    exports: [
+        FormsModule,
+        ReactiveFormsModule,
+        CommonModule,
+        MatCardModule,
+        MatButtonModule,
+        MatSlideToggleModule,
+        MatListModule,
+    ],
 })
 export class MaterialModule {}
