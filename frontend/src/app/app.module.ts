80af8a2922d5b3251c09ffff25be6ea141900d5a
Hekeem <79774156+hekeemje@users.noreply.github.com>
2022-08-08 10:56:14 +0300

    Angular material added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { environment } from '@env/environment';
 import { SharedModule } from '@shared/shared.module';
 
@@ -16,6 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
         AppRoutingModule,
         AngularFireModule.initializeApp(environment.firebase),
         AngularFireAuthModule,
+        BrowserAnimationsModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
