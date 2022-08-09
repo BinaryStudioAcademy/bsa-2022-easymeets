26c9cc527aa5b99eeef032800e7a7f832d34cb05
Evgen <bezvalko@gmail.com>
2022-08-09 22:14:00 +0300

    Add html and sass

--- frontend/src/app/modules/user/user-profile-page/user-profile-page.component.ts
+++ frontend/src/app/modules/user/user-profile-page/user-profile-page.component.ts
@@ -1,13 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-user-profile-page',
     templateUrl: './user-profile-page.component.html',
     styleUrls: ['./user-profile-page.component.sass'],
 })
-export class UserProfilePageComponent implements OnInit {
-    constructor() { }
-
-    ngOnInit(): void {
-    }
-}
+export class UserProfilePageComponent {}
