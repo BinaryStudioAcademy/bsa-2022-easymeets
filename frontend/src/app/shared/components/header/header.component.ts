1f5fb45d647e53f088234e90f6f4fa03a10ce179
Koval Vadym <vzlobinkov@gmail.com>
2022-08-09 19:19:00 +0300

    Change some CSS and add click on dropdowns

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.sass'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+    onSelectDropdown(id: number): void {
+        const dropdownDetailsDiv: HTMLElement = document.getElementById(`dropdown-content-${id}`) as HTMLElement;
+
+        dropdownDetailsDiv.style.display = dropdownDetailsDiv.style.display === 'block' ? 'none' : 'block';
+    }
+}
