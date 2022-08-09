4b666003ab2c88e653fe9c36abc6322abf5e713b
YuraRiabov <uraryabov@gmail.com>
2022-08-09 18:57:57 +0300

    Main settings added to general component

--- frontend/src/app/modules/availability/new-slot/general/general.component.ts
+++ frontend/src/app/modules/availability/new-slot/general/general.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
     styleUrls: ['./general.component.sass'],
 })
 export class GeneralComponent {
+    public slotSizes: string[] = ['30 min', '60 min'];
 
+    public slotsFrequencies: string[] = ['30 min', '60 min'];
+
+    public meetingLocations: string[] = ['Google Meet', 'Zoom'];
+
+    public selectedSlotSize: string = this.slotSizes[0];
+
+    public selectedFrequency: string = this.slotsFrequencies[0];
+
+    public selectedLocation: string = this.meetingLocations[0];
+
+    public meetingName: string;
 }
