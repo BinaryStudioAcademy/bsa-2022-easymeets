4c8390b4916ef76859a4b087f9418d52299276c1
YuraRiabov <uraryabov@gmail.com>
2022-08-09 23:27:01 +0300

    Interface for general settings created and used

---
+++ frontend/src/app/core/models/generalAvailabilitySettings.ts
@@ -0,0 +1,21 @@
+export interface GeneralAvailabilitySettings {
+    slotSize: string;
+
+    slotFrequency: string;
+
+    meetingLocation: string;
+
+    meetingPadding: string;
+
+    slotActivityOption: string;
+
+    slotActivityValue: string;
+
+    meetingName: string;
+
+    maxBookings: number;
+
+    minBookingMeetingDifference: string;
+
+    hideFromCommon: boolean;
+}
--- frontend/src/app/modules/availability/new-slot/general/general.component.ts
+++ frontend/src/app/modules/availability/new-slot/general/general.component.ts
@@ -1,42 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GeneralAvailabilitySettings } from '@core/models/generalAvailabilitySettings';
 
 @Component({
     selector: 'app-general',
     templateUrl: './general.component.html',
     styleUrls: ['./general.component.sass'],
 })
-export class GeneralComponent {
-    public slotSizes: string[] = ['30 min', '60 min'];
+export class GeneralComponent implements OnInit {
+    public settings: GeneralAvailabilitySettings;
 
-    public selectedSlotSize: string = this.slotSizes[0];
+    public slotSizes: string[] = ['30 min', '60 min'];
 
     public slotsFrequencies: string[] = ['30 min', '60 min'];
 
-    public selectedFrequency: string = this.slotsFrequencies[0];
-
     public meetingLocations: string[] = ['Google Meet', 'Zoom'];
-
-    public selectedLocation: string = this.meetingLocations[0];
 
     public meetingPaddings: string[] = ['15 min', '30 min'];
 
-    public selectedMeetingPadding: string = this.meetingPaddings[0];
-
     public slotActivityOptions: string[] = ['Days', 'Range', 'Indefinitely'];
-
-    public selectedSlotActivity: string = this.slotActivityOptions[0];
-
-    public slotActivityValue: string;
-
-    public meetingName: string;
-
-    public maxBookings: number;
 
     public minBookingMeetingDifferences: string[] = ['2 hours', '4 hours'];
 
-    public selectedBookingMeetingDifference: string = this.minBookingMeetingDifferences[0];
-
-    public hideFromCommon: boolean = false;
-
     public addAdvanced: boolean = true;
+
+    ngOnInit(): void {
+        this.settings = {
+            hideFromCommon: false,
+            maxBookings: 1,
+            meetingName: '',
+            slotActivityValue: '',
+            slotSize: this.slotSizes[0],
+            slotFrequency: this.slotsFrequencies[0],
+            meetingLocation: this.meetingLocations[0],
+            meetingPadding: this.meetingPaddings[0],
+            slotActivityOption: this.slotActivityOptions[0],
+            minBookingMeetingDifference: this.minBookingMeetingDifferences[0],
+        };
+    }
 }
