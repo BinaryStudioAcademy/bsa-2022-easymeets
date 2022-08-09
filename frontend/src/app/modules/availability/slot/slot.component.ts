636fa14e3f9071711db7c0df9a2910faadd27ffe
Olena Kozak <olena.s.kozak@gmail.com>
2022-08-09 13:18:00 +0300

    Fix bugs with displaying avatar list

--- frontend/src/app/modules/availability/slot/slot.component.ts
+++ frontend/src/app/modules/availability/slot/slot.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-slot',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./slot.component.sass'],
 })
 export class SlotComponent implements OnInit {
+    @Input() public slot: { time: string; user: string; link: string; meetingPlace: string; avatars: Array<string> };
+
     // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
     ngOnInit(): void {}
 }
