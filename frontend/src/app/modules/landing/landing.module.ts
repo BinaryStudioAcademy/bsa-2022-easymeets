60c8359189693fd4c4d8b58e8cf2c47e9bb14bec
Hekeem <79774156+hekeemje@users.noreply.github.com>
2022-08-09 20:59:39 +0300

    add angular material models

--- frontend/src/app/modules/landing/landing.module.ts
+++ frontend/src/app/modules/landing/landing.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '@shared/material/material.module';
 import { SharedModule } from '@shared/shared.module';
 
 import { LandingPageComponent } from './landing-page/landing-page.component';
@@ -6,6 +7,6 @@ import { LandingRoutingModule } from './landing-routing.module';
 
 @NgModule({
     declarations: [LandingPageComponent],
-    imports: [SharedModule, LandingRoutingModule],
+    imports: [SharedModule, LandingRoutingModule, MaterialModule],
 })
 export class LandingModule {}
