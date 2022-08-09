5073bdb222d2a5efbcd37373028d57ddb48e9263
Evgen <bezvalko@gmail.com>
2022-08-09 16:25:58 +0300

    Create user module and user profile page component

---
+++ frontend/src/app/modules/user/user-profile-page/user-profile-page.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-user-profile-page',
+    templateUrl: './user-profile-page.component.html',
+    styleUrls: ['./user-profile-page.component.sass'],
+})
+export class UserProfilePageComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+}
