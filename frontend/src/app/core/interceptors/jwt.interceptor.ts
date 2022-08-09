3a5aa2a49e5b292bb1d926a772a320844822fadf
OleksandrSlakvychBSA <oleksandr.slakvych.bsa@gmail.com>
2022-08-09 22:48:07 +0300

    fix output types

--- frontend/src/app/core/interceptors/jwt.interceptor.ts
+++ frontend/src/app/core/interceptors/jwt.interceptor.ts
@@ -1,19 +1,19 @@
 /* eslint-disable no-param-reassign */
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AuthService } from '@core/services/auth.service';
-import { from, lastValueFrom, Observable } from 'rxjs';
+import { from, lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
     // eslint-disable-next-line no-empty-function
     constructor(private authService: AuthService) {}
 
-    public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    public intercept(req: HttpRequest<any>, next: HttpHandler) {
         return from(this.handleAccess(req, next));
     }
 
-    private async handleAccess(req: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
+    private async handleAccess(req: HttpRequest<any>, next: HttpHandler) {
         const accessToken = await this.authService.getCurrentToken();
 
         if (accessToken) {
