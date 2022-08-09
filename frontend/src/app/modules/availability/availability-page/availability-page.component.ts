0b03bf6cd784947fc4c50947aaa16445278cf78a
Olena Kozak <olena.s.kozak@gmail.com>
2022-08-09 20:33:40 +0300

    Remove unused interface implementation

--- frontend/src/app/modules/availability/availability-page/availability-page.component.ts
+++ frontend/src/app/modules/availability/availability-page/availability-page.component.ts
@@ -1,11 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-availability-page',
     templateUrl: './availability-page.component.html',
     styleUrls: ['./availability-page.component.sass'],
 })
-export class AvailabilityPageComponent implements OnInit {
-    // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
-    ngOnInit(): void {}
-}
+export class AvailabilityPageComponent {}
