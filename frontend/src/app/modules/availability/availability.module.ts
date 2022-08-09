caac8c4a0a853d76d1847e66e2b06198dbcb40a3
YuraRiabov <uraryabov@gmail.com>
2022-08-09 18:57:23 +0300

    Material module added to availability module

--- frontend/src/app/modules/availability/availability.module.ts
+++ frontend/src/app/modules/availability/availability.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '@shared/material/material.module';
 
 import { GeneralComponent } from './new-slot/general/general.component';
 
@@ -9,6 +10,7 @@ import { GeneralComponent } from './new-slot/general/general.component';
     ],
     imports: [
         CommonModule,
+        MaterialModule,
     ],
 })
 export class AvailabilityModule { }
