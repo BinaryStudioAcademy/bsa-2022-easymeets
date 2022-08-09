6cfc8b67db73da0241ec1a05e26e2e0bbb557a7a
Olena Kozak <olena.s.kozak@gmail.com>
2022-08-09 11:19:26 +0300

    Change avatar size

--- frontend/src/app/modules/availability/availability-page/availability-page.component.ts
+++ frontend/src/app/modules/availability/availability-page/availability-page.component.ts
@@ -8,4 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class AvailabilityPageComponent implements OnInit {
     // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
     ngOnInit(): void {}
+
+    slots: Array<{ time: string; user: string; link: string; meetingPlace: string }> = [
+        { time: '30 min', user: 'Heorhii Matviichuk', link: 'Link.com/heorhii', meetingPlace: 'Meet' },
+        { time: '1 hour', user: 'Heorhii Matviichuk', link: 'Link.com/heorhii', meetingPlace: 'Zoom' },
+        { time: '15 min', user: 'Me & 1 Booker', link: 'Link.com/heorhii', meetingPlace: 'Zoom' },
+        { time: '30 min', user: 'Heorhii Matviichuk', link: 'Link.com/heorhii', meetingPlace: 'Meet' },
+    ];
 }
