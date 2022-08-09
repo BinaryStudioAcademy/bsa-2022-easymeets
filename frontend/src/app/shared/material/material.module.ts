084f0e2064ac2ebfa4d8e4c5ce3127298c1fe69f
YuraRiabov <uraryabov@gmail.com>
2022-08-09 18:56:15 +0300

    Material modules needed for general component added to material module

--- frontend/src/app/shared/material/material.module.ts
+++ frontend/src/app/shared/material/material.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
     declarations: [],
-    imports: [CommonModule, FormsModule, ReactiveFormsModule],
-    exports: [FormsModule, ReactiveFormsModule, CommonModule],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, MatSelectModule, MatInputModule],
+    exports: [FormsModule, ReactiveFormsModule, CommonModule, MatSelectModule, MatInputModule],
 })
 export class MaterialModule {}
