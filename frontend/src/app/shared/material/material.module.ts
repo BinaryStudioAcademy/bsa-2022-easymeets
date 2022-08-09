af2f206d1b8ba4dae3940beb8094cf7aa4193462
YuraRiabov <uraryabov@gmail.com>
2022-08-09 19:46:34 +0300

    Devider added in material module

--- frontend/src/app/shared/material/material.module.ts
+++ frontend/src/app/shared/material/material.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
     declarations: [],
-    imports: [CommonModule, FormsModule, ReactiveFormsModule, MatSelectModule, MatInputModule],
-    exports: [FormsModule, ReactiveFormsModule, CommonModule, MatSelectModule, MatInputModule],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, MatSelectModule, MatInputModule, MatDividerModule],
+    exports: [FormsModule, ReactiveFormsModule, CommonModule, MatSelectModule, MatInputModule, MatDividerModule],
 })
 export class MaterialModule {}
