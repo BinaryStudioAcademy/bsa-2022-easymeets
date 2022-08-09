2bdb0e5e33ea97eca2d707a5cc6b07d179bc212b
Koval Vadym <vzlobinkov@gmail.com>
2022-08-09 22:09:45 +0300

    Add click on pop-up/dropdown

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
+    // eslint-disable-next-line @angular-eslint/no-host-metadata-property
+    host: {
+        '(document:click)': 'onClick($event)',
+    },
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.sass'],
 })
 export class HeaderComponent {
-    onSelectDropdown(id: number): void {
+    // eslint-disable-next-line no-empty-function
+    constructor(private _eref: ElementRef) {}
+
+    onSelectDropdown(id: number, outsideCall: boolean = false): void {
         const dropdownDetailsDiv: HTMLElement = document.getElementById(`dropdown-content-${id}`) as HTMLElement;
 
-        dropdownDetailsDiv.style.display = dropdownDetailsDiv.style.display === 'block' ? 'none' : 'block';
+        if (outsideCall) {
+            dropdownDetailsDiv.style.display = dropdownDetailsDiv.style.display === 'block' ? 'none' : 'none';
+        } else {
+            dropdownDetailsDiv.style.display = dropdownDetailsDiv.style.display === 'block' ? 'none' : 'block';
+        }
+    }
+
+    onClick(event: Event) {
+        if (!this._eref.nativeElement.contains(event.target)) {
+            this.onSelectDropdown(1, true);
+            this.onSelectDropdown(2, true);
+        }
     }
 }
