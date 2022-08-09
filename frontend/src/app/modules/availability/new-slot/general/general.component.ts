649946e344725264471295b63a986949a676d000
YuraRiabov <uraryabov@gmail.com>
2022-08-09 21:05:57 +0300

    4/6 inputs of advanced settings added

--- frontend/src/app/modules/availability/new-slot/general/general.component.ts
+++ frontend/src/app/modules/availability/new-slot/general/general.component.ts
@@ -8,17 +8,33 @@ import { Component } from '@angular/core';
 export class GeneralComponent {
     public slotSizes: string[] = ['30 min', '60 min'];
 
-    public slotsFrequencies: string[] = ['30 min', '60 min'];
-
-    public meetingLocations: string[] = ['Google Meet', 'Zoom'];
-
     public selectedSlotSize: string = this.slotSizes[0];
+
+    public slotsFrequencies: string[] = ['30 min', '60 min'];
 
     public selectedFrequency: string = this.slotsFrequencies[0];
 
+    public meetingLocations: string[] = ['Google Meet', 'Zoom'];
+
     public selectedLocation: string = this.meetingLocations[0];
 
+    public meetingPaddings: string[] = ['15 min', '30 min'];
+
+    public selectedMeetingPadding: string = this.meetingPaddings[0];
+
+    public slotActivityOptions: string[] = ['Days', 'Range', 'Indefinitely'];
+
+    public selectedSlotActivity: string = this.slotActivityOptions[0];
+
+    public slotActivityValue: string;
+
     public meetingName: string;
+
+    public maxBookings: number;
+
+    public minBookingMeetingDifferences: string[] = ['2 hours', '4 hours'];
+
+    public selectedBookingMeetingDifference: string = this.minBookingMeetingDifferences[0];
 
     public addAdvanced: boolean = true;
 }
