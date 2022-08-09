a70ed3d7ad17fb669cfe73073d2edd237781b1bb
YuraRiabov <uraryabov@gmail.com>
2022-08-09 21:05:25 +0300

    Radio module added to material module

--- frontend/src/app/shared/material/material.module.ts
+++ frontend/src/app/shared/material/material.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
     declarations: [],
-    imports: [CommonModule, FormsModule, ReactiveFormsModule, MatSelectModule, MatInputModule, MatDividerModule],
-    exports: [FormsModule, ReactiveFormsModule, CommonModule, MatSelectModule, MatInputModule, MatDividerModule],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, MatSelectModule, MatInputModule, MatDividerModule, MatRadioModule],
+    exports: [FormsModule, ReactiveFormsModule, CommonModule, MatSelectModule, MatInputModule, MatDividerModule, MatRadioModule],
 })
 export class MaterialModule {}
