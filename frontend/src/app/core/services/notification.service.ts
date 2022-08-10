610e149edb83d60acc9d30acc5ba84b577e4b722
OleksandrSlakvychBSA <oleksandr.slakvych.bsa@gmail.com>
2022-08-10 09:40:53 +0300

    add notification service

---
+++ frontend/src/app/core/services/notification.service.ts
@@ -0,0 +1,17 @@
+import { inject, Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({ providedIn: 'root' })
+export class NotificationService {
+    public constructor(private snackBar: MatSnackBar) {
+        this.snackBar = inject(MatSnackBar);
+    }
+
+    public showErrorMessage(error: any) {
+        this.snackBar.open(error, '', { duration: 3000, panelClass: 'error-snack-bar' });
+    }
+
+    public showUsualMessage(message: any) {
+        this.snackBar.open(message, '', { duration: 3000, panelClass: 'usual-snack-bar' });
+    }
+}
--- frontend/src/app/shared/material/material.module.ts
+++ frontend/src/app/shared/material/material.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
     declarations: [],
-    imports: [CommonModule, FormsModule, ReactiveFormsModule],
-    exports: [FormsModule, ReactiveFormsModule, CommonModule],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, MatButtonModule, MatSnackBarModule],
+    exports: [FormsModule, ReactiveFormsModule, CommonModule, MatButtonModule, MatSnackBarModule],
 })
 export class MaterialModule {}
