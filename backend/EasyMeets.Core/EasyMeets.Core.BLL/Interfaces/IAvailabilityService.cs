326b764234408e44b9e43df105812738390f01fc
Olena Kozak <olena.s.kozak@gmail.com>
2022-08-10 17:21:20 +0300

    Add method to interface IAvailabilityService

--- backend/EasyMeets.Core/EasyMeets.Core.BLL/Interfaces/IAvailabilityService.cs
+++ backend/EasyMeets.Core/EasyMeets.Core.BLL/Interfaces/IAvailabilityService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EasyMeets.Core.DAL.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 namespace EasyMeets.Core.BLL.Interfaces
 {
     public interface IAvailabilityService
-    { 
+    {
+        public Task<ICollection<AvailabilitySlot>> GetAllAvailabilitySlotsAsync();
     }
 }
