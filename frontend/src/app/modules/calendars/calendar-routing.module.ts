4b509fc3f590c0be4c991925d0d4fb1504822710
Vasiliy <vasiokpavluyk@gmail.com>
2022-08-10 12:12:03 +0300

    route to calendars added

--- frontend/src/app/modules/calendars/calendar-routing.module.ts
+++ frontend/src/app/modules/calendars/calendar-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { CalendarsPageComponent } from './calendars-page/calendars-page.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: CalendarsPageComponent,
+    },
+];
 
 @NgModule({
-    declarations: [],
-    imports: [],
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
 export class CalendarRoutingModule {}
