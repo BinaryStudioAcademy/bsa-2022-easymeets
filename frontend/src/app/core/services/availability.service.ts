5cb6d7432d76399895d9e7dc17fcf88844352188
Olena Kozak <olena.s.kozak@gmail.com>
2022-08-10 17:41:09 +0300

    Add base getAvailabilitySlots method

---
+++ frontend/src/app/core/services/availability.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { AvailabilitySlot } from '@core/models/availiability-slot';
+
+import { HttpInternalService } from './http-internal.service';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AvailabilityService {
+    public routePrefix = '/availability';
+
+    // eslint-disable-next-line no-empty-function
+    constructor(private httpService: HttpInternalService) {}
+
+    public getAvailabilitySlots() {
+        return this.httpService.getRequest<AvailabilitySlot[]>(`${this.routePrefix}`);
+    }
+}
--- frontend/src/app/modules/availability/availability-page/availability-page.component.ts
+++ frontend/src/app/modules/availability/availability-page/availability-page.component.ts
@@ -1,8 +1,34 @@
 import { Component } from '@angular/core';
+import { AvailabilitySlot } from '@core/models/availiability-slot';
+import { AvailabilityService } from '@core/services/availability.service';
 
 @Component({
     selector: 'app-availability-page',
     templateUrl: './availability-page.component.html',
     styleUrls: ['./availability-page.component.sass'],
 })
-export class AvailabilityPageComponent {}
+export class AvailabilityPageComponent {
+    public availabilitySlots: AvailabilitySlot[];
+
+    public loadingSlots: boolean;
+
+    // eslint-disable-next-line no-empty-function
+    constructor(private availabilitySlotService: AvailabilityService) {}
+
+    public getAvailabilitySlots() {
+        this.loadingSlots = true;
+        this.availabilitySlotService
+            .getAvailabilitySlots()
+            .subscribe(
+                (resp) => {
+                    this.loadingSlots = false;
+                    this.availabilitySlots = resp;
+                },
+                (error) => {
+                    this.loadingSlots = false;
+                    // eslint-disable-next-line no-console
+                    console.log(error);
+                },
+            );
+    }
+}
