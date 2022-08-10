b04c4750582e9bb975bcc6fa9c00fa9936f92cc0
OleksandrSlakvychBSA <oleksandr.slakvych.bsa@gmail.com>
2022-08-10 12:29:10 +0300

    fix app component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,25 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
-import { NotificationService } from '@core/services/notification.service';
 import { SpinnerService } from '@core/services/spinner.service';
 
 @Component({
     selector: 'app-root',
     template: '<app-loading-spinner></app-loading-spinner><router-outlet></router-outlet>',
 })
-export class AppComponent implements OnInit {
-    constructor(
-        private router: Router,
-        private spinner: SpinnerService,
-        private notificationService: NotificationService,
-    ) {
+export class AppComponent {
+    constructor(private router: Router, private spinner: SpinnerService) {
         this.listenRouter();
-    }
-
-    public ngOnInit(): void {
-        console.log('FFFFFFF');
-        //this.notificationService.showUsualMessage('Successfully updated');
-        this.notificationService.showErrorMessage('Image has invalid dimension');
     }
 
     private listenRouter() {
