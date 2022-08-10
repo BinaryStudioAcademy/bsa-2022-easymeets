37f11e1dbf5e824c2cef8f8f1cb95ddaabef27e3
Koval Vadym <vzlobinkov@gmail.com>
2022-08-10 12:19:01 +0300

    Change dropdown`s id to enum

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,11 @@
+/* eslint-disable indent */
 import { Component, ElementRef } from '@angular/core';
+
+// eslint-disable-next-line no-shadow
+export enum DropDownTypes {
+    TeamsDD,
+    AccountDD,
+}
 
 @Component({
     // eslint-disable-next-line @angular-eslint/no-host-metadata-property
@@ -13,8 +20,28 @@ export class HeaderComponent {
     // eslint-disable-next-line no-empty-function
     constructor(private _eref: ElementRef) {}
 
-    onSelectDropdown(id: number, outsideCall: boolean = false): void {
-        const dropdownDetailsDiv: HTMLElement = document.getElementById(`dropdown-content-${id}`) as HTMLElement;
+    public DropDownTypesEnum = DropDownTypes;
+
+    onSelectDropdown(type: DropDownTypes, outsideCall: boolean = false): void {
+        let dropdownId: string = '';
+
+        switch (type) {
+            case DropDownTypes.AccountDD: {
+                dropdownId += 'account';
+                break;
+            }
+            case DropDownTypes.TeamsDD: {
+                dropdownId += 'teams';
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+
+        const dropdownDetailsDiv: HTMLElement = document.getElementById(
+            `dropdown-content-${dropdownId}`,
+        ) as HTMLElement;
 
         if (outsideCall) {
             dropdownDetailsDiv.style.display = dropdownDetailsDiv.style.display === 'block' ? 'none' : 'none';
@@ -25,8 +52,8 @@ export class HeaderComponent {
 
     onClick(event: Event) {
         if (!this._eref.nativeElement.contains(event.target)) {
-            this.onSelectDropdown(1, true);
-            this.onSelectDropdown(2, true);
+            this.onSelectDropdown(DropDownTypes.AccountDD, true);
+            this.onSelectDropdown(DropDownTypes.TeamsDD, true);
         }
     }
 }
