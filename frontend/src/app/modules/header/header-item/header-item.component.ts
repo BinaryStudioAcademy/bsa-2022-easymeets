fa7d7e177425f28f6fdc56343e08aa53d92222d1
Koval Vadym <vzlobinkov@gmail.com>
2022-08-11 21:05:22 +0300

    Customize CSS for Angular Material Select

--- frontend/src/app/modules/header/header-item/header-item.component.ts
+++ frontend/src/app/modules/header/header-item/header-item.component.ts
@@ -1,59 +1,8 @@
-/* eslint-disable indent */
-import { Component, ElementRef } from '@angular/core';
-
-// eslint-disable-next-line no-shadow
-export enum DropDownTypes {
-    TeamsDD,
-    AccountDD,
-}
+import { Component } from '@angular/core';
 
 @Component({
-    // eslint-disable-next-line @angular-eslint/no-host-metadata-property
-    host: {
-        '(document:click)': 'onClick($event)',
-    },
     selector: 'app-header-item',
     templateUrl: './header-item.component.html',
     styleUrls: ['./header-item.component.sass'],
 })
-export class HeaderItemComponent {
-    // eslint-disable-next-line no-empty-function
-    constructor(private _eref: ElementRef) {}
-
-    public DropDownTypesEnum = DropDownTypes;
-
-    onSelectDropdown(type: DropDownTypes, outsideCall: boolean = false): void {
-        let dropdownId: string = '';
-
-        switch (type) {
-            case DropDownTypes.AccountDD: {
-                dropdownId += 'account';
-                break;
-            }
-            case DropDownTypes.TeamsDD: {
-                dropdownId += 'teams';
-                break;
-            }
-            default: {
-                break;
-            }
-        }
-
-        const dropdownDetailsDiv: HTMLElement = document.getElementById(
-            `dropdown-content-${dropdownId}`,
-        ) as HTMLElement;
-
-        if (outsideCall) {
-            dropdownDetailsDiv.style.display = dropdownDetailsDiv.style.display === 'block' ? 'none' : 'none';
-        } else {
-            dropdownDetailsDiv.style.display = dropdownDetailsDiv.style.display === 'block' ? 'none' : 'block';
-        }
-    }
-
-    onClick(event: Event) {
-        if (!this._eref.nativeElement.contains(event.target)) {
-            this.onSelectDropdown(DropDownTypes.AccountDD, true);
-            this.onSelectDropdown(DropDownTypes.TeamsDD, true);
-        }
-    }
-}
+export class HeaderItemComponent {}
