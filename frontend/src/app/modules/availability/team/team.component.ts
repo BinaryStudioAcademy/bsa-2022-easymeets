ef812119f5eb6cd8664d02dd4df260e495af0eb1
Olena Kozak <olena.s.kozak@gmail.com>
2022-08-12 01:55:04 +0300

    Fix bug with displaying copy icon during loading user slots

--- frontend/src/app/modules/availability/team/team.component.ts
+++ frontend/src/app/modules/availability/team/team.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AvailabilitySlot } from '@core/models/availiability-slot';
 import { TeamWithSlots } from '@core/models/team-with-slot';
+import { SpinnerService } from '@core/services/spinner.service';
 
 @Component({
     selector: 'app-team',
@@ -11,6 +12,9 @@ export class TeamComponent implements OnInit {
     @Input() public team: TeamWithSlots;
 
     public slots: Array<AvailabilitySlot>;
+
+    // eslint-disable-next-line no-empty-function
+    constructor(public spinnerService: SpinnerService) {}
 
     ngOnInit(): void {
         this.slots = this.team.availabilitySlots;
--- frontend/src/app/modules/availability/user-slot/user-slot.component.ts
+++ frontend/src/app/modules/availability/user-slot/user-slot.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { AvailabilitySlot } from '@core/models/availiability-slot';
 import { User } from '@core/models/user';
+import { SpinnerService } from '@core/services/spinner.service';
 
 @Component({
     selector: 'app-user-slot',
@@ -11,4 +12,7 @@ export class UserSlotComponent {
     @Input() public userSlots: Array<AvailabilitySlot>;
 
     @Input() public currentUser: User;
+
+    // eslint-disable-next-line no-empty-function
+    constructor(public spinnerService: SpinnerService) {}
 }
