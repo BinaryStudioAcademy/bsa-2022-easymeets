90ad26b86e4c68a1c901d3e5df98fd67110ce5c5
OleksandrSlakvychBSA <oleksandr.slakvych.bsa@gmail.com>
2022-08-12 00:18:03 +0300

    fix warnings

--- backend/EasyMeets.Core/EasyMeets.Core.Common/DTO/User/UserDto.cs
+++ backend/EasyMeets.Core/EasyMeets.Core.Common/DTO/User/UserDto.cs
@@ -6,13 +6,13 @@ namespace EasyMeets.Core.Common.DTO.User
     public class UserDto
     {
         public long Id { get; set; }
-        public string Image { get; set; }
-        public string UserName { get; set; }      
+        public string? Image { get; set; }
+        public string? UserName { get; set; }      
         public Language Language { get; set; }
         public TimeFormat TimeFormat { get; set; }
         public DateFormat DateFormat { get; set; }
         public Country Country { get; set; }
-        public string Phone { get; set; }
+        public string? Phone { get; set; }
         public TimeZone TimeZone { get; set; }
     }
 }
