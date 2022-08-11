af9808db7d493da76efaa51b51c3b8e46e9aa1a7
Vasiliy <vasiokpavluyk@gmail.com>
2022-08-11 14:42:40 +0300

    removed unnecessary fields in helpers

--- frontend/src/app/core/helpers/users-calendar-helper.ts
+++ frontend/src/app/core/helpers/users-calendar-helper.ts
@@ -4,43 +4,31 @@ export const getDefaultCalendars = (): IUserCalendar[] => [
     {
         id: 1,
         email: 'email@email.com',
-        teamsFor: undefined,
-        teamFrom: undefined,
         checkForConflicts: false,
     },
     {
         id: 2,
         email: 'email@email.com',
-        teamsFor: undefined,
-        teamFrom: undefined,
         checkForConflicts: false,
     },
     {
         id: 3,
         email: 'email@email.com',
-        teamsFor: undefined,
-        teamFrom: undefined,
         checkForConflicts: false,
     },
     {
         id: 4,
         email: 'email@email.com',
-        teamsFor: undefined,
-        teamFrom: undefined,
         checkForConflicts: false,
     },
     {
         id: 5,
         email: 'email@email.com',
-        teamsFor: undefined,
-        teamFrom: undefined,
         checkForConflicts: false,
     },
     {
         id: 6,
         email: 'email@email.com',
-        teamsFor: undefined,
-        teamFrom: undefined,
         checkForConflicts: false,
     },
 ];
