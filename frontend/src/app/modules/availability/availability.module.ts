a19e5a20e7b0513e35aa74d979bd7f8f5a6bd664
YuraRiabov <uraryabov@gmail.com>
2022-08-11 18:58:52 +0300

    New availability component created

--- frontend/src/app/modules/availability/availability.module.ts
+++ frontend/src/app/modules/availability/availability.module.ts
@@ -5,12 +5,20 @@ import { MaterialModule } from '@shared/material/material.module';
 import { AvailabilityPageComponent } from './availability-page/availability-page.component';
 import { EventDetailComponent } from './new-slot/event-detail/event-detail.component';
 import { GeneralComponent } from './new-slot/general/general.component';
+import { NewAvailabilityComponent } from './new-slot/new-availability/new-availability.component';
 import { SlotComponent } from './slot/slot.component';
 import { TeamComponent } from './team/team.component';
 import { AvailabilityRoutingModule } from './availability-routing.module';
 
 @NgModule({
-    declarations: [AvailabilityPageComponent, SlotComponent, TeamComponent, GeneralComponent, EventDetailComponent],
+    declarations: [
+        AvailabilityPageComponent,
+        SlotComponent,
+        TeamComponent,
+        GeneralComponent,
+        EventDetailComponent,
+        NewAvailabilityComponent,
+    ],
     imports: [CommonModule, AvailabilityRoutingModule, MaterialModule],
 })
 export class AvailabilityModule {}
