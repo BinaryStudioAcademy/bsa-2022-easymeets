04cc16379b5d5d98bb9bdd5f268ee72fc5a43d78
Hekeem <79774156+hekeemje@users.noreply.github.com>
2022-08-11 15:36:15 +0300

    created IUploadFileInterface

---
+++ backend/EasyMeets.Core/EasyMeets.Core.BLL/Interfaces/IUploadFileService.cs
@@ -0,0 +1,18 @@
+﻿using Azure.Storage.Blobs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMeets.Core.BLL.Interfaces
+{
+    internal interface IUploadFileService
+    {
+        public Task<BlobInfo> GetBlobAsync(string name);
+        public Task<IEnumerable<string>> ListBlobsAsync();
+        public Task UploadFileBlobAsync(string filePath, string fileName);
+        public Task UploadContentBlobAsync(string content, string fileName);
+        public Task DeleteBlobAsync(string blobName);
+    }
+}
