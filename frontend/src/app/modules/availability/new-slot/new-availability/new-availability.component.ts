e2c41a110294af3dbb0c8baef586819bb6bc29d6
YuraRiabov <uraryabov@gmail.com>
2022-08-11 21:06:29 +0300

    Side menu group added to new availability component

--- frontend/src/app/modules/availability/new-slot/new-availability/new-availability.component.ts
+++ frontend/src/app/modules/availability/new-slot/new-availability/new-availability.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SideMenuGroup } from '@core/interfaces/sideMenu/sideMenuGroup';
 
 @Component({
     selector: 'app-new-availability',
     templateUrl: './new-availability.component.html',
     styleUrls: ['./new-availability.component.sass'],
 })
-export class NewAvailabilityComponent {
+export class NewAvailabilityComponent implements OnInit {
+    public sideMenuGroup: SideMenuGroup;
 
+    ngOnInit(): void {
+        this.initializeSideMenu();
+    }
+
+    private initializeSideMenu() {
+        this.sideMenuGroup = {
+            items: [
+                { text: 'General', routerLink: 'general' },
+                { text: 'Booking page', routerLink: 'event-details' },
+            ],
+        };
+    }
 }
