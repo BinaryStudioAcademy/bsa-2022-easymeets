188a23b0dac82841d3e32c3a15deaada4db19652
YuraRiabov <uraryabov@gmail.com>
2022-08-13 02:51:45 +0300

    Validator for advanced settings dto added

---
+++ backend/EasyMeets.Core/EasyMeets.Core.WebAPI/Validators/Availability/NewAvailability/NewAdvancedSlotSettingsDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using EasyMeets.Core.Common.DTO.Availability.NewAvailability;
+using FluentValidation;
+
+namespace EasyMeets.Core.WebAPI.Validators.Availability.NewAvailability;
+
+public class NewAdvancedSlotSettingsDtoValidator : AbstractValidator<NewAdvancedSlotSettingsDto>
+{
+    public NewAdvancedSlotSettingsDtoValidator()
+    {
+        RuleFor(s => s.Days).Must(d => d > 0);
+        RuleFor(s => s.PaddingMeeting).Must(p => p > 0);
+        RuleFor(s => s.MaxNumberOfBookings).Must(n => n > 0);
+        RuleFor(s => s.MinBookingMeetingDifference).Must(d => d > 0);
+    }
+}
