1d302f4a018034002898c91df8280fd1271dafe6
YuraRiabov <uraryabov@gmail.com>
2022-08-13 00:08:27 +0300

    Extension method for email validation added

--- backend/EasyMeets.Core/EasyMeets.Core.Common/Validation/ValidationExtensions.cs
+++ backend/EasyMeets.Core/EasyMeets.Core.Common/Validation/ValidationExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace EasyMeets.Core.Common.Validation;
+﻿using System.Text.RegularExpressions;
+
+namespace EasyMeets.Core.Common.Validation;
 
 public static class ValidationExtensions
 {
@@ -6,5 +8,10 @@ public static class ValidationExtensions
     {
         string[] validLocations = { "Google Meet", "Zoom", "Offline" };
         return validLocations.Contains(value);
+    }
+
+    public static bool IsValidEmail(this string value)
+    {
+        return Regex.IsMatch(value, @"^([a-zA-z0-9]+([._\-][a-zA-z0-9]+)?)+@([a-zA-z0-9]+([.\-][a-zA-Z0-9]+)?)+\.[a-zA-Z]{2,4}$");
     }
 }
