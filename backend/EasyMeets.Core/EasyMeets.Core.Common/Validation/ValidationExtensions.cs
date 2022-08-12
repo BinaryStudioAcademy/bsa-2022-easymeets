ec617ade5b965aa15e56c44bd623e01ff64047da
YuraRiabov <uraryabov@gmail.com>
2022-08-13 01:34:34 +0300

    Extension method for validating phone number in format "+xx (xxx) xxx xx xx" added

--- backend/EasyMeets.Core/EasyMeets.Core.Common/Validation/ValidationExtensions.cs
+++ backend/EasyMeets.Core/EasyMeets.Core.Common/Validation/ValidationExtensions.cs
@@ -21,4 +21,10 @@ public static class ValidationExtensions
         var pattern = @"^[\w\d]+[\w\d\- ]+$";
         return Regex.IsMatch(value, pattern);
     }
+
+    public static bool IsValidPhoneNumber(this string value)
+    {
+        var pattern = @"^\+\d{1,2} \(\d{3}\) \d{3} \d{2} \d{2}$";
+        return Regex.IsMatch(value, pattern);
+    }
 }
