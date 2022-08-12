52cd588976df25951266dbae71465d118eddd9bc
YuraRiabov <uraryabov@gmail.com>
2022-08-12 23:04:48 +0300

    String extension to check if it's a valid meeting location added

---
+++ backend/EasyMeets.Core/EasyMeets.Core.Common/Validation/ValidationExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyMeets.Core.Common.Validation;
+
+public static class ValidationExtensions
+{
+    public static bool IsValidMeetingLocation(this string value)
+    {
+        string[] validLocations = { "Google Meet", "Zoom", "Offline" };
+        return validLocations.Contains(value);
+    }
+}
