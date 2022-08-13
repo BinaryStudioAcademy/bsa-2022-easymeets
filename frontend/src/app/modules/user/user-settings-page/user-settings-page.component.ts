2045d089eb20ffa69931e5b9170071c55e22e4e5
Vasiliy <vasiokpavluyk@gmail.com>
2022-08-13 11:11:21 +0300

    created component for settings page

---
+++ frontend/src/app/modules/user/user-settings-page/user-settings-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-user-settings-page',
+    templateUrl: './user-settings-page.component.html',
+    styleUrls: ['./user-settings-page.component.sass'],
+})
+export class UserSettingsPageComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
--- frontend/src/app/modules/user/user.module.ts
+++ frontend/src/app/modules/user/user.module.ts
@@ -3,10 +3,12 @@ import { UserRoutingModule } from '@modules/user/user-routing.module';
 import { SharedModule } from '@shared/shared.module';
 
 import { UserProfilePageComponent } from './user-profile-page/user-profile-page.component';
+import { UserSettingsPageComponent } from './user-settings-page/user-settings-page.component';
 
 @NgModule({
     declarations: [
         UserProfilePageComponent,
+        UserSettingsPageComponent,
     ],
     imports: [
         SharedModule,
