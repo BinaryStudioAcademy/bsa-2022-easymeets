b212ea80be1ceab49db6f87cd66decd391087254
Vasiliy <vasiokpavluyk@gmail.com>
2022-08-13 14:35:38 +0300

    changes in styles for sidebar

--- frontend/src/app/modules/user/user-routing.module.ts
+++ frontend/src/app/modules/user/user-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
                 component: UserProfilePageComponent,
             },
             {
-                path: 'integrations/calendar',
+                path: 'integrations/calendars',
                 component: CalendarsPageComponent,
             },
         ],
