740e4636b2db996c40c075243f319c80e233f2e3
Olena Kozak <olena.s.kozak@gmail.com>
2022-08-13 21:29:42 +0300

    Fix bug with filtering AvailabilitySlotsGroupByTeams

--- backend/EasyMeets.Core/EasyMeets.Core.Common/DTO/Availability/AvailabilitySlotMemberDto.cs
+++ backend/EasyMeets.Core/EasyMeets.Core.Common/DTO/Availability/AvailabilitySlotMemberDto.cs
@@ -1,6 +1,8 @@
-﻿namespace EasyMeets.Core.Common.DTO.Availability
+﻿using EasyMeets.Core.Common.DTO.Common;
+
+namespace EasyMeets.Core.Common.DTO.Availability
 {
-    public class AvailabilitySlotMemberDto
+    public class AvailabilitySlotMemberDto:EntityDto<long>
     {
         public string? MemberImage { get; set; }
         public string? MemberUserName { get; set; }
