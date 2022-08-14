5b5e23b6f8077cdf0ea299b042b5ebf772b212e4
YuraRiabov <uraryabov@gmail.com>
2022-08-14 13:21:29 +0300

    Regular expressions moved to separate dictionary

--- backend/EasyMeets.Core/EasyMeets.Core.Common/Validation/ValidationExtensions.cs
+++ backend/EasyMeets.Core/EasyMeets.Core.Common/Validation/ValidationExtensions.cs
@@ -4,6 +4,14 @@ namespace EasyMeets.Core.Common.Validation;
 
 public static class ValidationExtensions
 {
+    public static IReadOnlyDictionary<string, string> RegularExpressions = new Dictionary<string, string>
+    {
+        { "Email", @"^([a-zA-z0-9]+([._\-][a-zA-z0-9]+)?)+@([a-zA-z0-9]+([.\-][a-zA-Z0-9]+)?)+\.[a-zA-Z]{2,4}$" },
+        { "Username", @"^[\w\d]+[\w\d\- ]+$" },
+        { "PhoneNumber", @"^\+\d{1,2} \(\d{3}\) \d{3} \d{2} \d{2}$" },
+        { "TeamLink", @"^/[a-zA-Z\d\-]+\d*$" },
+        { "Password", @"^[^ ]+$" }
+    };
     public static bool IsValidMeetingLocation(this string value)
     {
         string[] validLocations = { "Google Meet", "Zoom", "Offline" };
@@ -12,31 +20,31 @@ public static class ValidationExtensions
 
     public static bool IsValidEmail(this string value)
     {
-        var pattern = @"^([a-zA-z0-9]+([._\-][a-zA-z0-9]+)?)+@([a-zA-z0-9]+([.\-][a-zA-Z0-9]+)?)+\.[a-zA-Z]{2,4}$";
+        var pattern = RegularExpressions["Email"];
         return Regex.IsMatch(value, pattern);
     }
 
     public static bool IsValidUsername(this string value)
     {
-        var pattern = @"^[\w\d]+[\w\d\- ]+$";
+        var pattern = RegularExpressions["Username"];
         return Regex.IsMatch(value, pattern);
     }
 
     public static bool IsValidPhoneNumber(this string value)
     {
-        var pattern = @"^\+\d{1,2} \(\d{3}\) \d{3} \d{2} \d{2}$";
+        var pattern = RegularExpressions["PhoneNumber"];
         return Regex.IsMatch(value, pattern);
     }
 
     public static bool IsValidTeamLink(this string value)
     {
-        var pattern = @"^/[a-zA-Z\d\-]+\d*$";
+        var pattern = RegularExpressions["TeamLink"];
         return Regex.IsMatch(value, pattern);
     }
 
     public static bool IsValidPassword(this string value)
     {
-        var pattern = @"^[^ ]+$";
+        var pattern = RegularExpressions["Password"];
         return Regex.IsMatch(value, pattern);
     }
 }
