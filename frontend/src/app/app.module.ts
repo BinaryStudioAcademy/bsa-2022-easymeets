a987df88a5ee788940f3593014b62ca7f691f7c9
OleksandrSlakvychBSA <oleksandr.slakvych.bsa@gmail.com>
2022-08-15 20:50:23 +0300

    small fixes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from '@core/core.module';
 import { environment } from '@env/environment';
-import { HeaderModule } from '@modules/header/header.module';
 import { MaterialModule } from '@shared/material/material.module';
 import { SharedModule } from '@shared/shared.module';
 
@@ -19,7 +18,6 @@ import { AppRoutingModule } from './app-routing.module';
         BrowserAnimationsModule,
         CoreModule,
         SharedModule,
-        HeaderModule,
         AppRoutingModule,
         AngularFireModule.initializeApp(environment.firebase),
         AngularFireAuthModule,
