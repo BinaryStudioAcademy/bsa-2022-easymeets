21af8a920f0f853194655407f707ee5de366fff9
Koval Vadym <vzlobinkov@gmail.com>
2022-08-15 15:32:18 +0300

    Add HTML of external booking page

--- frontend/src/app/modules/external-booking/external-booking.module.ts
+++ frontend/src/app/modules/external-booking/external-booking.module.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '@shared/material/material.module';
 import { SharedModule } from '@shared/shared.module';
 
 import { ExternalBookingPageComponent } from './external-booking-page/external-booking-page.component';
@@ -6,6 +8,6 @@ import { ExternalBookingRoutingModule } from './external-booking-routing.module'
 
 @NgModule({
     declarations: [ExternalBookingPageComponent],
-    imports: [SharedModule, ExternalBookingRoutingModule],
+    imports: [SharedModule, ExternalBookingRoutingModule, CommonModule, MaterialModule],
 })
 export class ExternalBookingModule {}
