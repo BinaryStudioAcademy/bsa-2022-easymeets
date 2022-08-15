f6e2c51c95f62a2034ef3e6f6a059a28d6a48aef
Koval Vadym <vzlobinkov@gmail.com>
2022-08-15 17:47:42 +0300

    Add user to page

--- frontend/src/app/modules/external-booking/external-booking-page/external-booking-page.component.ts
+++ frontend/src/app/modules/external-booking/external-booking-page/external-booking-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { IUser } from '@core/models/IUser';
 import { SpinnerService } from '@core/services/spinner.service';
+import { UserService } from '@core/services/user.service';
 
 @Component({
     selector: 'app-external-booking-page',
@@ -7,6 +9,14 @@ import { SpinnerService } from '@core/services/spinner.service';
     styleUrls: ['./external-booking-page.component.sass'],
 })
 export class ExternalBookingPageComponent {
+    public selectedUser: IUser;
+
+    public selectedUserId: number = 3;
+
     // eslint-disable-next-line no-empty-function
-    constructor(public spinnerService: SpinnerService) {}
+    constructor(public spinnerService: SpinnerService, private userService: UserService) {
+        this.userService.getCurrentUserById(this.selectedUserId).subscribe((user) => {
+            this.selectedUser = user;
+        });
+    }
 }
