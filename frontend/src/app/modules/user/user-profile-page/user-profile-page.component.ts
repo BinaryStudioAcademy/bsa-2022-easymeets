522b360cb968ddae1326f43e2ae64bbbf03f7bc0
Olena Kozak <olena.s.kozak@gmail.com>
2022-08-15 17:08:37 +0300

    Add load image method to user-profile-page.component.ts Add cursor: pointer style to upload-picture-button class Add load file input to user-profile-page.component.html

--- frontend/src/app/modules/user/user-profile-page/user-profile-page.component.ts
+++ frontend/src/app/modules/user/user-profile-page/user-profile-page.component.ts
@@ -5,4 +5,39 @@ import { Component } from '@angular/core';
     templateUrl: './user-profile-page.component.html',
     styleUrls: ['./user-profile-page.component.sass'],
 })
-export class UserProfilePageComponent {}
+export class UserProfilePageComponent {
+    public imageUrl: string;
+
+    public imageFile: File;
+
+    public loadImage(target: any) {
+        // eslint-disable-next-line no-debugger
+        debugger;
+        // eslint-disable-next-line prefer-destructuring
+        this.imageFile = target.files[0];
+
+        if (!this.imageFile) {
+            target.value = '';
+
+            return;
+        }
+
+        if (this.imageFile.size / 1000000 > 5) {
+            target.value = '';
+            console.log('Image can\'t be heavier than ~5MB');
+        }
+
+        const reader = new FileReader();
+
+        reader.addEventListener(
+            'load',
+            () => {
+                // eslint-disable-next-line no-debugger
+                debugger;
+                this.imageUrl = reader.result as string;
+                console.log(this.imageUrl);
+            },
+        );
+        reader.readAsDataURL(this.imageFile);
+    }
+}
