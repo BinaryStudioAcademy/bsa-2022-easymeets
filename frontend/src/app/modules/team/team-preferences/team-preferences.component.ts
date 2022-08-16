bdba82c514067bafaf6a0f9c84887ca793b7d81f
Evgen <bezvalko@gmail.com>
2022-08-16 21:37:07 +0300

    Create team module and team preferences component

---
+++ frontend/src/app/modules/team/team-preferences/team-preferences.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-team-preferences',
+    templateUrl: './team-preferences.component.html',
+    styleUrls: ['./team-preferences.component.sass'],
+})
+export class TeamPreferencesComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+}
---
+++ frontend/src/app/modules/team/team.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { TeamPreferencesComponent } from './team-preferences/team-preferences.component';
+
+@NgModule({
+    declarations: [
+        TeamPreferencesComponent,
+    ],
+    imports: [
+        CommonModule,
+    ],
+})
+export class TeamModule { }
