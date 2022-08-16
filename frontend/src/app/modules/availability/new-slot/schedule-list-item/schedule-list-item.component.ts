bbee0eb727fd86a184a539dc733e172e801ff18e
YuraRiabov <uraryabov@gmail.com>
2022-08-16 22:14:32 +0300

    Changing schedule item on change implemented

--- frontend/src/app/modules/availability/new-slot/schedule-list-item/schedule-list-item.component.ts
+++ frontend/src/app/modules/availability/new-slot/schedule-list-item/schedule-list-item.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { IScheduleItem } from '@core/models/IScheduleItem';
 
 @Component({
     selector: 'app-schedule-list-item',
     templateUrl: './schedule-list-item.component.html',
     styleUrls: ['./schedule-list-item.component.sass'],
 })
-export class ScheduleListItemComponent {
+export class ScheduleListItemComponent implements OnInit {
+    @Input() public item: IScheduleItem;
+
+    @Output() public itemChanged: EventEmitter<IScheduleItem> = new EventEmitter<IScheduleItem>();
+
+    @Input() public displayDay: string;
+
+    public startValue: string;
+
+    public endValue: string;
+
+    public onDateChange($event: any, isStart: boolean) {
+        const hours: string = $event.target.value.substring(0, 2);
+        const minutes: string = $event.target.value.substring(3, 5);
+
+        if (isStart) {
+            this.item.start.setHours(parseInt(hours, 10));
+            this.item.start.setMinutes(parseInt(minutes, 10));
+        } else {
+            this.item.end.setHours(parseInt(hours, 10));
+            this.item.end.setMinutes(parseInt(minutes, 10));
+        }
+        this.itemChanged.emit(this.item);
+    }
+
+    ngOnInit(): void {
+        this.startValue = `${this.item.start.getHours()}:${this.item.start.getMinutes()}`;
+        this.endValue = `${this.item.end.getHours()}:${this.item.end.getMinutes()}`;
+    }
 }
