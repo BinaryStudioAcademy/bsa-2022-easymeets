a58243f06012c9546daa2a681a3824f660e89bf9
Koval Vadym <vzlobinkov@gmail.com>
2022-08-16 22:27:01 +0300

    Fix input field

--- frontend/src/app/modules/external-booking/external-booking-page/external-booking-page.component.ts
+++ frontend/src/app/modules/external-booking/external-booking-page/external-booking-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { IExtendBookingSideMenu } from '@core/models/IExtendBookingSideMenu';
 import { IUser } from '@core/models/IUser';
 import { SpinnerService } from '@core/services/spinner.service';
 import { UserService } from '@core/services/user.service';
@@ -9,7 +10,7 @@ import { UserService } from '@core/services/user.service';
     styleUrls: ['./external-booking-page.component.sass'],
 })
 export class ExternalBookingPageComponent {
-    @Input() groups: SideMenuGroup[];
+    @Input() menu: IExtendBookingSideMenu;
 
     public selectedUser: IUser;
 
