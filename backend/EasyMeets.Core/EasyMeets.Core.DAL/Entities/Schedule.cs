67929d6be7e26d78304c6b9053f51d7356356365
YuraRiabov <uraryabov@gmail.com>
2022-08-16 16:34:06 +0300

    Schedule config added

--- backend/EasyMeets.Core/EasyMeets.Core.DAL/Entities/Schedule.cs
+++ backend/EasyMeets.Core/EasyMeets.Core.DAL/Entities/Schedule.cs
@@ -1,11 +1,9 @@
-﻿using TimeZone = EasyMeets.Core.Common.Enums.TimeZone;
-
-namespace EasyMeets.Core.DAL.Entities;
+﻿namespace EasyMeets.Core.DAL.Entities;
 
 public class Schedule : Entity<long>
 {
     public long AvailabilitySlotId { get; set; }
-    public TimeZone TimeZone { get; set; }
+    public int TimeZone { get; set; }
     public bool WithTeamMembers { get; set; }
 
     public AvailabilitySlot AvailabilitySlot { get; set; } = null!;
