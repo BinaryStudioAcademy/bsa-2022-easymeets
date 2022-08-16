b4bcd2bd44730cb4aa198220055c07e47bed0e54
YuraRiabov <uraryabov@gmail.com>
2022-08-16 20:41:09 +0300

    Schedule list view component removed

--- frontend/src/app/modules/availability/new-slot/schedule/schedule.component.ts
+++ frontend/src/app/modules/availability/new-slot/schedule/schedule.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { getScheduleItems } from '@core/helpers/schedule-list-helper';
+import { IScheduleItem } from '@core/models/IScheduleItem';
 
 @Component({
     selector: 'app-schedule',
     templateUrl: './schedule.component.html',
     styleUrls: ['./schedule.component.sass'],
 })
-export class ScheduleComponent {
+export class ScheduleComponent implements OnInit {
+    @Input() public items: IScheduleItem[];
 
+    @Output() public itemsChange: EventEmitter<IScheduleItem[]> = new EventEmitter<IScheduleItem[]>();
+
+    ngOnInit(): void {
+        if (this.items === undefined) {
+            this.items = getScheduleItems();
+        }
+    }
 }
