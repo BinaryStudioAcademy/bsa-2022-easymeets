a924153913b4b7d6039138c8bea12f4eadd05c5c
Hekeem <79774156+hekeemje@users.noreply.github.com>
2022-08-16 18:54:08 +0300

    create bookings-page component

---
+++ frontend/src/app/modules/bookings/bookings-page/bookings-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-bookings-page',
+    templateUrl: './bookings-page.component.html',
+    styleUrls: ['./bookings-page.component.sass'],
+})
+export class BookingsPageComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
