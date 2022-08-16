b5adaee6f3336cee23ceba04e46fd790998b4369
YuraRiabov <uraryabov@gmail.com>
2022-08-16 19:45:08 +0300

    Getting and returning items logic implemented in schedule list view component

--- frontend/src/app/modules/availability/new-slot/schedule-list-view/schedule-list-view.component.ts
+++ frontend/src/app/modules/availability/new-slot/schedule-list-view/schedule-list-view.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { getScheduleItems } from '@core/helpers/schedule-list-helper';
+import { IScheduleItem } from '@core/models/IScheduleItem';
 
 @Component({
     selector: 'app-schedule-list-view',
     templateUrl: './schedule-list-view.component.html',
     styleUrls: ['./schedule-list-view.component.sass'],
 })
-export class ScheduleListViewComponent {
+export class ScheduleListViewComponent implements OnInit {
+    @Input() public items: IScheduleItem[];
+
+    @Output() public itemsChange: EventEmitter<IScheduleItem[]> = new EventEmitter<IScheduleItem[]>();
+
+    ngOnInit(): void {
+        if (this.items === undefined) {
+            this.items = getScheduleItems();
+        }
+    }
 }
