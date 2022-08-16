2a7fb90298be067ff445f9247e6871751eaa6672
YuraRiabov <uraryabov@gmail.com>
2022-08-16 10:15:43 +0300

    Schedule list view component created

--- frontend/src/app/modules/availability/availability.module.ts
+++ frontend/src/app/modules/availability/availability.module.ts
@@ -9,6 +9,7 @@ import { EventDetailComponent } from './new-slot/event-detail/event-detail.compo
 import { GeneralComponent } from './new-slot/general/general.component';
 import { NewAvailabilityComponent } from './new-slot/new-availability/new-availability.component';
 import { ScheduleComponent } from './new-slot/schedule/schedule.component';
+import { ScheduleListViewComponent } from './new-slot/schedule-list-view/schedule-list-view.component';
 import { SlotComponent } from './slot/slot.component';
 import { TeamComponent } from './team/team.component';
 import { UserSlotComponent } from './user-slot/user-slot.component';
@@ -24,6 +25,7 @@ import { AvailabilityRoutingModule } from './availability-routing.module';
         EventDetailComponent,
         NewAvailabilityComponent,
         ScheduleComponent,
+        ScheduleListViewComponent,
     ],
     imports: [CommonModule, AvailabilityRoutingModule, MaterialModule, SideMenuModule, HeaderModule],
 })
---
+++ frontend/src/app/modules/availability/new-slot/schedule-list-view/schedule-list-view.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-schedule-list-view',
+    templateUrl: './schedule-list-view.component.html',
+    styleUrls: ['./schedule-list-view.component.sass'],
+})
+export class ScheduleListViewComponent {
+}
