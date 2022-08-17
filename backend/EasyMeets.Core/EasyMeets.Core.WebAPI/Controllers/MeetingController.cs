3e01ff8a196273759b1f672aaa0347be2bf2df60
Hekeem <79774156+hekeemje@users.noreply.github.com>
2022-08-17 18:53:19 +0300

    Enable Autorize for MeetingController

--- backend/EasyMeets.Core/EasyMeets.Core.WebAPI/Controllers/MeetingController.cs
+++ backend/EasyMeets.Core/EasyMeets.Core.WebAPI/Controllers/MeetingController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EasyMeets.Core.WebAPI.Controllers
 {
-    //[Authorize]
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class MeetingController : ControllerBase
