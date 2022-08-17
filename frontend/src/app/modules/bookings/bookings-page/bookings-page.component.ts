02bc399f718ae1102fc6819447a085a630ed1bf7
Hekeem <79774156+hekeemje@users.noreply.github.com>
2022-08-17 20:22:51 +0300

    implement meeting booking service

--- frontend/src/app/modules/bookings/bookings-page/bookings-page.component.ts
+++ frontend/src/app/modules/bookings/bookings-page/bookings-page.component.ts
@@ -1,12 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from '@core/base/base.component';
+import { IMeetingBooking } from '@core/models/IMeetingBooking';
+import { MeetingBookingsService } from '@core/services/meeting-bookings.service';
 
 @Component({
     selector: 'app-bookings-page',
     templateUrl: './bookings-page.component.html',
     styleUrls: ['./bookings-page.component.sass'],
 })
-export class BookingsPageComponent implements OnInit {
-    constructor() {}
+export class BookingsPageComponent extends BaseComponent implements OnInit {
+    constructor(public meetingService: MeetingBookingsService) {
+        super();
+        this.meetings = [];
+    }
 
-    ngOnInit(): void {}
+    public meetings: IMeetingBooking[];
+
+    public ngOnInit(): void {
+        this.meetingService
+            .getMeetingsForBookings()
+            .pipe(this.untilThis)
+            .subscribe(
+                (resp: IMeetingBooking[]) => {
+                    this.meetings = resp;
+                    console.log(this.meetings);
+                },
+                () => {},
+            );
+
+        console.log(this.meetings);
+    }
 }
