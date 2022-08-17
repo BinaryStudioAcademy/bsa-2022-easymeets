beab8dc11f3ae58ffe9c6644d76570f78a487f83
YuraRiabov <uraryabov@gmail.com>
2022-08-17 22:50:21 +0300

    New event details for new availability added

---
+++ frontend/src/app/core/models/INewEventDetails.ts
@@ -0,0 +1,10 @@
+export interface INewEventDetails {
+    link: string;
+    welcomeMessage: string;
+    language: string;
+    bookingsPerDay: number;
+    allowToAddGuests: boolean;
+    passwordProtectionIsUsed: boolean;
+    passwordProtection: string | null;
+    timeZoneVisibility: boolean;
+}
