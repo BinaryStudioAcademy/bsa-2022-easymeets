b520605500d66cc641ba6d0c95d8407289b9e7e6
Hekeem <79774156+hekeemje@users.noreply.github.com>
2022-08-17 14:41:39 +0300

    created and implemented MeetingService

---
+++ backend/EasyMeets.Core/EasyMeets.Core.BLL/Interfaces/IMeetingService.cs
@@ -0,0 +1,9 @@
+﻿using EasyMeets.Core.DAL.Entities;
+
+namespace EasyMeets.Core.BLL.Interfaces
+{
+    public interface IMeetingService
+    {
+        public Task<List<Meeting>> GetMeetingsAsync();
+    }
+}
---
+++ backend/EasyMeets.Core/EasyMeets.Core.BLL/Services/MeetingService.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using EasyMeets.Core.BLL.Interfaces;
+using EasyMeets.Core.DAL.Context;
+using EasyMeets.Core.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyMeets.Core.BLL.Services
+{
+    public class MeetingService : BaseService, IMeetingService
+    {
+        public MeetingService(EasyMeetsCoreContext context, IMapper mapper) : base(context, mapper) { }
+
+        public async Task<List<Meeting>> GetMeetingsAsync()
+        {
+            var meetings = await _context.Meetings.ToListAsync();
+
+            return meetings;
+        }
+    }
+}
