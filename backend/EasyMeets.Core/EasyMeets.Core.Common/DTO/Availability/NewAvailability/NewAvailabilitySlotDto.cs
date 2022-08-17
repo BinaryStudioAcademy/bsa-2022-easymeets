711812c780f1f24fe94a47a9e0a329d3c7e5d69f
YuraRiabov <uraryabov@gmail.com>
2022-08-17 23:13:03 +0300

    Bool for indicating if slot has advanced settings added to new availability dto

--- backend/EasyMeets.Core/EasyMeets.Core.Common/DTO/Availability/NewAvailability/NewAvailabilitySlotDto.cs
+++ backend/EasyMeets.Core/EasyMeets.Core.Common/DTO/Availability/NewAvailability/NewAvailabilitySlotDto.cs
@@ -3,6 +3,7 @@
 public class NewAvailabilitySlotDto
 {
     public long TeamId { get; set; }
+    public bool HasAdvancedSettings { get; set; }
     public GeneralDetailsDto? GeneralDetails { get; set; }
     public EventDetailsDto? EventDetails { get; set; }
     public NewAdvancedSlotSettingsDto? AdvancedSettings { get; set; }
