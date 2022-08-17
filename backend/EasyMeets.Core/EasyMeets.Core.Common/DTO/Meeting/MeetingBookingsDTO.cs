be30b1054744c6ee0fd2826743e97f01cee384c2
Hekeem <79774156+hekeemje@users.noreply.github.com>
2022-08-17 17:12:19 +0300

    added meetingDTO

---
+++ backend/EasyMeets.Core/EasyMeets.Core.Common/DTO/Meeting/MeetingBookingsDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyMeets.Core.Common.DTO.Meeting
+{
+    public class MeetingBookingsDTO
+    {
+        public string MeetingTime { get; set; }
+        public string MeetingTitle { get; set; }
+        public string MeetingDuration { get; set; }
+        public string MembersTitle { get; set; }
+    }
+}
---
+++ frontend/src/app/core/models/IMeeting.ts
@@ -0,0 +1,7 @@
+export interface IMeeting {
+    meetingTitle: string,
+    meetingTime: string,
+    meetingDuration: string,
+    membersTitle: string
+    membersList:
+}
