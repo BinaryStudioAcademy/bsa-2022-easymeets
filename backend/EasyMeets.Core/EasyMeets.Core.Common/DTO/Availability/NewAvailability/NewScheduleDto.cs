f62a5ad06a7f76cb76f11bc9fcdb621ae6e7c460
YuraRiabov <uraryabov@gmail.com>
2022-08-18 09:12:09 +0300

    New schedule and new schedule item dtos added

---
+++ backend/EasyMeets.Core/EasyMeets.Core.Common/DTO/Availability/NewAvailability/NewScheduleDto.cs
@@ -0,0 +1,8 @@
+﻿namespace EasyMeets.Core.Common.DTO.Availability.NewAvailability;
+
+public class NewScheduleDto
+{
+    public int TimeZone { get; set; }
+    public bool WithTeamMembers { get; set; }
+    public List<NewScheduleItemDto> Items { get; set; }
+}
