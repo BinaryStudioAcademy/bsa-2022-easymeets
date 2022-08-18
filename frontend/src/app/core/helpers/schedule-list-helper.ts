b5d0f3ccea2e4fc2392577fd4702b5c852c4149e
YuraRiabov <uraryabov@gmail.com>
2022-08-18 10:06:47 +0300

    Schedule list helper values fixed to match timespan format

--- frontend/src/app/core/helpers/schedule-list-helper.ts
+++ frontend/src/app/core/helpers/schedule-list-helper.ts
@@ -1,45 +1,45 @@
-import { IScheduleItem } from '@core/models/IScheduleItem';
+import { IScheduleItem } from '@core/models/new-availability-slot/IScheduleItem';
 
 export const getScheduleItems = (): IScheduleItem[] => [
     {
-        start: new Date(2022, 1, 1, 13, 0),
-        end: new Date(2022, 1, 1, 15, 0),
+        start: '13:00:00',
+        end: '15:00:00',
         weekDay: 6,
         isEnabled: false,
     },
     {
-        start: new Date(2022, 1, 1, 13, 0),
-        end: new Date(2022, 1, 1, 15, 0),
+        start: '13:00:00',
+        end: '15:00:00',
         weekDay: 0,
         isEnabled: true,
     },
     {
-        start: new Date(2022, 1, 1, 13, 0),
-        end: new Date(2022, 1, 1, 15, 0),
+        start: '13:00:00',
+        end: '15:00:00',
         weekDay: 1,
         isEnabled: true,
     },
     {
-        start: new Date(2022, 1, 1, 13, 0),
-        end: new Date(2022, 1, 1, 15, 0),
+        start: '13:00:00',
+        end: '15:00:00',
         weekDay: 2,
         isEnabled: true,
     },
     {
-        start: new Date(2022, 1, 1, 13, 0),
-        end: new Date(2022, 1, 1, 15, 0),
+        start: '13:00:00',
+        end: '15:00:00',
         weekDay: 3,
         isEnabled: true,
     },
     {
-        start: new Date(2022, 1, 1, 13, 0),
-        end: new Date(2022, 1, 1, 15, 0),
+        start: '13:00:00',
+        end: '15:00:00',
         weekDay: 4,
         isEnabled: true,
     },
     {
-        start: new Date(2022, 1, 1, 13, 0),
-        end: new Date(2022, 1, 1, 15, 0),
+        start: '13:00:00',
+        end: '15:00:00',
         weekDay: 5,
         isEnabled: false,
     },
