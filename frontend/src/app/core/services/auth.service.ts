e2a06c54d52a2218da20f0d0135d831377bd2702
YuraRiabov <uraryabov@gmail.com>
2022-08-18 20:43:17 +0300

    Blocking users without verified email fixed

--- frontend/src/app/core/services/auth.service.ts
+++ frontend/src/app/core/services/auth.service.ts
@@ -64,7 +64,7 @@ export class AuthService {
         }
         const userData = JSON.parse(currentUser!) as User;
 
-        return userData.emailVerified;
+        return userData !== null;
     }
 
     public getCurrentToken() {
