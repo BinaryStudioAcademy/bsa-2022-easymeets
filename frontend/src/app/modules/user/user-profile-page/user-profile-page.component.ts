00a85431af5c5714e42fa85f056ca51a09097694
Evgen <bezvalko@gmail.com>
2022-08-18 16:37:04 +0300

    Fix doube space

--- frontend/src/app/modules/user/user-profile-page/user-profile-page.component.ts
+++ frontend/src/app/modules/user/user-profile-page/user-profile-page.component.ts
@@ -43,7 +43,7 @@ export class UserProfilePageComponent extends BaseComponent implements OnInit {
         Validators.required,
         Validators.minLength(2),
         Validators.maxLength(50),
-        Validators.pattern(/^[іІїЇa-zA-Z\dа-яА-Я- ]*$/),
+        Validators.pattern(/^[іІїЇa-zA-Z\dа-яА-Я-]+(\s|)[іІїЇa-zA-Z\dа-яА-Я-]*$/),
     ]);
 
     public phoneControl: FormControl = new FormControl('', [Validators.required, Validators.minLength(10)]);
