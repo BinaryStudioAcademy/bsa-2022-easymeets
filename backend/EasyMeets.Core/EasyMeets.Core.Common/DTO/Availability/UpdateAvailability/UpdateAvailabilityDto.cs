72d902ef17f077f3065a2604fcfff8a584ecab20
YuraRiabov <uraryabov@gmail.com>
2022-08-18 13:55:41 +0300

    Schedule added to availability and update availability dtos

--- backend/EasyMeets.Core/EasyMeets.Core.Common/DTO/Availability/UpdateAvailability/UpdateAvailabilityDto.cs
+++ backend/EasyMeets.Core/EasyMeets.Core.Common/DTO/Availability/UpdateAvailability/UpdateAvailabilityDto.cs
@@ -1,9 +1,12 @@
-﻿namespace EasyMeets.Core.Common.DTO.Availability.UpdateAvailability;
+﻿using EasyMeets.Core.Common.DTO.Availability.Schedule;
+
+namespace EasyMeets.Core.Common.DTO.Availability.UpdateAvailability;
 
 public class UpdateAvailabilityDto
 {
     public GeneralDetailsUpdateDto GeneralDetailsUpdate { get; set; } = null!;
     public EventDetailsUpdateDto EventDetailsUpdate { get; set; } = null!;
+    public ScheduleDto Schedule { get; set; } = null!;
     public bool HasAdvancedSettings { get; set; }
     public bool IsActive { get; set; }
 }
