2d6a41846d63502de98fd4bed0b3184662ca3765
YuraRiabov <uraryabov@gmail.com>
2022-08-18 20:59:52 +0300

    Checking if user is logged in logic fixed

--- frontend/src/app/core/services/auth.service.ts
+++ frontend/src/app/core/services/auth.service.ts
@@ -64,7 +64,7 @@ export class AuthService {
         }
         const userData = JSON.parse(currentUser!) as User;
 
-        return userData !== null;
+        return userData?.emailVerified;
     }
 
     public getCurrentToken() {
