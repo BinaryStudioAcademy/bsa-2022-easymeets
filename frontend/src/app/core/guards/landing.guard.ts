ed1f85157ff6493d97080f7d46a75fee8b401926
YuraRiabov <uraryabov@gmail.com>
2022-08-19 23:01:20 +0300

    Return type removed in canActivate in landing guard to make code cleaner

--- frontend/src/app/core/guards/landing.guard.ts
+++ frontend/src/app/core/guards/landing.guard.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router, UrlTree } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '@core/services/auth.service';
-import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
@@ -10,7 +9,7 @@ export class LandingGuard implements CanActivate {
     // eslint-disable-next-line no-empty-function
     constructor(private authService: AuthService, private router: Router) {}
 
-    canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    canActivate() {
         return !this.authService.isLoggedIn() ? true : this.router.parseUrl('/availability');
     }
 }
