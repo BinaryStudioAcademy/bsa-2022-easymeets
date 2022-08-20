36c608b5d70d570bffdb84362bca7ff100793801
Olena Kozak <olena.s.kozak@gmail.com>
2022-08-20 15:33:59 +0300

    Add base markup to new-meeting.component.html

--- frontend/src/app/modules/new-meeting/new-meeting.module.ts
+++ frontend/src/app/modules/new-meeting/new-meeting.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HeaderModule } from '@modules/header/header.module';
+import { SideMenuModule } from '@modules/side-menu/side-menu.module';
 import { MaterialModule } from '@shared/material/material.module';
 
 import { NewMeetingComponent } from './new-meeting/new-meeting.component';
@@ -12,7 +13,7 @@ import { NewMeetingRoutingModule } from './new-meeting-routing.module';
     ],
     imports: [
         CommonModule,
-        NewMeetingRoutingModule, MaterialModule, HeaderModule,
+        NewMeetingRoutingModule, MaterialModule, HeaderModule, SideMenuModule,
     ],
 })
 export class NewMeetingModule { }
