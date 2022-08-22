653cd72a73087132023fe8b01b82a76852e2230c
Olena Kozak <olena.s.kozak@gmail.com>
2022-08-22 18:50:00 +0300

    Fix bu with duplication controllers

--- backend/EasyMeets.Core/EasyMeets.Core.WebAPI/Controllers/MeetingController.cs
+++ backend/EasyMeets.Core/EasyMeets.Core.WebAPI/Controllers/MeetingController.cs
@@ -1,5 +1,7 @@
 ﻿using EasyMeets.Core.BLL.Interfaces;
+using EasyMeets.Core.BLL.Services;
 using EasyMeets.Core.Common.DTO.Meeting;
+using EasyMeets.Core.Common.DTO.Team;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,15 +13,26 @@ namespace EasyMeets.Core.WebAPI.Controllers
     public class MeetingController : ControllerBase
     {
         private readonly IMeetingService _meetingService;
+        private readonly ITeamService _teamService;
+        private readonly IUserService _userService;
 
-        public MeetingController(IMeetingService meetingService)
+        public MeetingController(IMeetingService meetingService, ITeamService teamService, IUserService userService)
         {
             _meetingService = meetingService;
+            _userService = userService;
+            _teamService = teamService;
         }
 
         [HttpGet]
         [Route("GetThreeMeetingMembers")]
         public async Task<List<MeetingThreeMembersDTO>> GetThreeMeetingMembersAsync() => await _meetingService.GetThreeMeetingMembersAsync();
 
+        [HttpGet]
+        public async Task<ActionResult<ICollection<NewMeetingTeamMemberDto>>> GetTeamMembersOfCurrentUser()
+        {
+            var user = await _userService.GetCurrentUserAsync();
+            var teamMembers = await _teamService.GetTeamMembersOfCurrentUserAsync(user.Id);
+            return Ok(teamMembers);
+        }  
     }
 }
