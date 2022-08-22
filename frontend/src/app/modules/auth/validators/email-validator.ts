e6756fae493799e9ec46d5e15fb5b51c1b263db6
Oleksandr <OlexandrTanasienko@gmail.com>
2022-08-22 12:57:09 +0300

    Return email check for sign up validator

--- frontend/src/app/modules/auth/validators/email-validator.ts
+++ frontend/src/app/modules/auth/validators/email-validator.ts
@@ -3,13 +3,6 @@ import { AuthService } from '@core/services/auth.service';
 import { map } from 'rxjs';
 
 export class EmailValidator {
-    static loginEmailValidator(authService: AuthService): AsyncValidatorFn {
-        return (control: AbstractControl) =>
-            authService
-                .checkEmail(control.value)
-                .pipe(map((result: boolean) => (result ? null : { userInNotExist: true })));
-    }
-
     static signUpEmailValidator(authService: AuthService): AsyncValidatorFn {
         return (control: AbstractControl) =>
             authService
