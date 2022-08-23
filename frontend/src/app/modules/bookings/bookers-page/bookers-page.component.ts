4d2a67af93c8eafb1b50decf60e0ce239d84ee30
YuraRiabov <uraryabov@gmail.com>
2022-08-23 12:13:55 +0300

    Booker and bookers page components created, booker component implemented and used in bookings page

---
+++ frontend/src/app/modules/bookings/bookers-page/bookers-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-bookers-page',
+    templateUrl: './bookers-page.component.html',
+    styleUrls: ['./bookers-page.component.sass'],
+})
+export class BookersPageComponent {
+}
