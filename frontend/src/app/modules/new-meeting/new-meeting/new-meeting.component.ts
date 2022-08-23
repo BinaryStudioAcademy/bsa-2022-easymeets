5aa236d9475216bfa250d834b9722e81f0ec4864
Olena Kozak <olena.s.kozak@gmail.com>
2022-08-23 14:40:44 +0300

    Add validation to meeting name and custom time fields

--- frontend/src/app/modules/new-meeting/new-meeting/new-meeting.component.ts
+++ frontend/src/app/modules/new-meeting/new-meeting/new-meeting.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { getDisplayDate } from '@core/helpers/date-helper';
 import { getDisplayDuration } from '@core/helpers/display-duration-hepler';
 import { INewMeetingTeamMember } from '@core/models/INewMeetingTeamMember';
 import { NewMeetingService } from '@core/services/new-meeting.service';
+import { meetingNameRegex, naturalNumberRegex } from '@shared/constants/model-validation';
 import { LocationType } from '@shared/enums/locationType';
 
 @Component({
@@ -11,7 +12,7 @@ import { LocationType } from '@shared/enums/locationType';
     templateUrl: './new-meeting.component.html',
     styleUrls: ['./new-meeting.component.sass'],
 })
-export class NewMeetingComponent {
+export class NewMeetingComponent implements OnInit {
     public teamMembers: INewMeetingTeamMember[];
 
     public durations = getDisplayDuration();
@@ -24,9 +25,28 @@ export class NewMeetingComponent {
 
     public locations = Object.keys(LocationType).filter(key => Number.isNaN(Number(key)));
 
+    public meetingNameControl: FormControl = new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(50),
+        Validators.pattern(meetingNameRegex),
+    ]);
+
+    public customTimeControl: FormControl = new FormControl('', [
+        Validators.required,
+        Validators.pattern(naturalNumberRegex),
+    ]);
+
     // eslint-disable-next-line no-unused-vars
     constructor(private newMeetingService: NewMeetingService) {
         this.getTeamMembersOfCurrentUser();
+    }
+
+    ngOnInit(): void {
+        this.meetingForm = new FormGroup({
+            meetingName: this.meetingNameControl,
+            customTime: this.customTimeControl,
+        });
     }
 
     public getTeamMembersOfCurrentUser() {
