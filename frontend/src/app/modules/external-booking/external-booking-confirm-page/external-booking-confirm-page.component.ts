b765a21abc24e5d24b0dc2415e6efb04a6988d26
Hekeem <79774156+hekeemje@users.noreply.github.com>
2022-08-24 23:43:49 +0300

    added validation for input fields

--- frontend/src/app/modules/external-booking/external-booking-confirm-page/external-booking-confirm-page.component.ts
+++ frontend/src/app/modules/external-booking/external-booking-confirm-page/external-booking-confirm-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { IExternalAnswers } from '@core/models/IExternalAnswers';
+import { userNameRegex } from '@shared/constants/model-validation';
 
 @Component({
     selector: 'app-external-booking-confirm-page',
@@ -16,6 +18,28 @@ export class ExternalBookingConfirmPageComponent implements OnInit {
             externalEmail: '',
             externalExtraInfo: '',
         };
+    }
+
+    public confirmForm = new FormGroup({
+        characters: new FormControl('', [
+            Validators.pattern(userNameRegex),
+            Validators.minLength(2),
+            Validators.maxLength(50),
+        ]),
+        email: new FormControl('', [Validators.email]),
+        extraInfo: new FormControl('', [Validators.minLength(3), Validators.maxLength(80)]),
+    });
+
+    get characters() {
+        return this.confirmForm.get('characters');
+    }
+
+    get email() {
+        return this.confirmForm.get('email');
+    }
+
+    get extraInfo() {
+        return this.confirmForm.get('extraInfo');
     }
 
     public externalAnswers: IExternalAnswers;
