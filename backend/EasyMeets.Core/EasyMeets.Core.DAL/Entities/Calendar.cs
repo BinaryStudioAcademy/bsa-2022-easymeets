60fb197d327309d7645b5b58a10f5ed4c64accba
Vasiliy <vasiokpavluyk@gmail.com>
2022-08-24 12:34:07 +0300

    fix github warnings

--- backend/EasyMeets.Core/EasyMeets.Core.DAL/Entities/Calendar.cs
+++ backend/EasyMeets.Core/EasyMeets.Core.DAL/Entities/Calendar.cs
@@ -10,10 +10,10 @@ public class Calendar : AuditEntity<long>
     public long UserId { get; set; }
     public long? AddEventsFromTeamId { get; set; }
     public bool CheckForConflicts { get; set; }
-    public string ConnectedCalendar { get; set; }
-    public string AccessToken { get; set; }
-    public string RefreshToken { get; set; }
-    public string Uid { get; set; }
+    public string ConnectedCalendar { get; set; } = string.Empty;
+    public string AccessToken { get; set; }  = string.Empty;
+    public string RefreshToken { get; set; }  = string.Empty;
+    public string Uid { get; set; }  = string.Empty;
 
     public User User { get; set; } = null!;
     public Team? ImportEventsFromTeam { get; set; }
