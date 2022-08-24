ed78853ec1308dfb0bcadbd36de3711ed025dfb5
YuraRiabov <uraryabov@gmail.com>
2022-08-24 23:56:18 +0300

    Video conferencing component added

---
+++ frontend/src/app/modules/settings/video-conferencing/video-conferencing.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-video-conferencing',
+    templateUrl: './video-conferencing.component.html',
+    styleUrls: ['./video-conferencing.component.sass'],
+})
+export class VideoConferencingComponent {
+}
