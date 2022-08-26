f8a4b7c172350169892187d5e740e658ae3d94fb
Olena Kozak <olena.s.kozak@gmail.com>
2022-08-27 02:49:59 +0300

    Remove wrong link

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    coreUrl: 'https://localhost:5050',
+    coreUrl: 'http://localhost:5050',
     notifierUrl: 'http://localhost:5070',
     firebase: {
         apiKey: 'AIzaSyAF5LeybBJklic1uhIpenfDYoNvuH3mSqY',
