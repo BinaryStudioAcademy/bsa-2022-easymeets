3712d3ec6cabc211b1392c0ff8e731009070366c
YuraRiabov <uraryabov@gmail.com>
2022-08-29 00:27:51 +0300

    Dashboard page enhancement with arrow and profile redirection

--- frontend/src/app/modules/availability/user-slot/user-slot.component.ts
+++ frontend/src/app/modules/availability/user-slot/user-slot.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Router } from '@angular/router';
 import { IAvailabilitySlot } from '@core/models/IAvailiabilitySlot';
 import { IUser } from '@core/models/IUser';
 import { SpinnerService } from '@core/services/spinner.service';
@@ -13,10 +14,16 @@ export class UserSlotComponent {
 
     @Input() public currentUser: IUser;
 
+    @Input() public showArrow: boolean;
+
     @Output() isReload = new EventEmitter<boolean>();
 
     // eslint-disable-next-line no-empty-function
-    constructor(public spinnerService: SpinnerService) {}
+    constructor(public spinnerService: SpinnerService, private router: Router) {}
+
+    goToPage(pageName: string) {
+        this.router.navigate([`${pageName}`]);
+    }
 
     isDeleted(isRemove: boolean) {
         this.isReload.emit(isRemove);
