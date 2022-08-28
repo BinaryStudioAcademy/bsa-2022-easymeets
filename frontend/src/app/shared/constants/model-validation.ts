bd13b8b847a1ef83ee52186b29e8235af5616036
Hekeem <79774156+hekeemje@users.noreply.github.com>
2022-08-28 20:56:45 +0300

    add hour minutes regex pattern

--- frontend/src/app/shared/constants/model-validation.ts
+++ frontend/src/app/shared/constants/model-validation.ts
@@ -2,3 +2,4 @@ export const userNameRegex: string | RegExp = new RegExp('^[a-zA-Zа-яА-Я0-9-
 export const meetingNameRegex = "^([A-Za-z0-9-']+\\s)*[A-Za-z0-9-']+$";
 export const naturalNumberRegex = '^[1-9][0-9]*$';
 export const timeNumberRegex = '\\d{2}:\\d{2}';
+export const hourMinutesRegex = '^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$';
