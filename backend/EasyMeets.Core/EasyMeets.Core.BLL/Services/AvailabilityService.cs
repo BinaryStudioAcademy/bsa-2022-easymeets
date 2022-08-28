a73c6ba34c65c875584b33823e3cafa1fbe5c401
OleksandrSlakvychBSA <oleksandr.slakvych.bsa@gmail.com>
2022-08-28 15:41:09 +0300

    small fix

--- backend/EasyMeets.Core/EasyMeets.Core.BLL/Services/AvailabilityService.cs
+++ backend/EasyMeets.Core/EasyMeets.Core.BLL/Services/AvailabilityService.cs
@@ -46,7 +46,7 @@ namespace EasyMeets.Core.BLL.Services
                         AuthorName = y.Author.Name,
                         TeamName = y.Team.Name,
                         LocationType = y.LocationType,
-                        EmailTemplateSettings = _mapper.Map<List<EmailTemplatesSettingsDto>>(y.EmailTemplates)
+                        EmailTemplateSettings = _mapper.Map<List<EmailTemplatesSettingsDto>>(y.EmailTemplates),
                         Members = _mapper.Map<ICollection<AvailabilitySlotMemberDto>>(y.SlotMembers),
                     }
                 )
