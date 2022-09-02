6c39b2a447b9929dcdda7c33538116ee94eb1ffc
Koval Vadym <vzlobinkov@gmail.com>
2022-09-02 18:53:42 +0300

    Make fixes

--- frontend/src/app/shared/components/booking-window/booking-window.component.ts
+++ frontend/src/app/shared/components/booking-window/booking-window.component.ts
@@ -1,9 +1,7 @@
 import { Component, EventEmitter, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { BaseComponent } from '@core/base/base.component';
 import { LocationTypeMapping } from '@core/helpers/location-type-mapping';
-import { INewMeetingMember } from '@core/models/INewMeetingTeamMember';
-import { LocationType } from '@shared/enums/locationType';
-import { IConfirmButtonOptions } from '@shared/models/confirmWindow/IConfirmButtonOptions';
 import { IConfirmDialogData } from '@shared/models/confirmWindow/IConfirmDialogData';
 
 @Component({
@@ -11,28 +9,8 @@ import { IConfirmDialogData } from '@shared/models/confirmWindow/IConfirmDialogD
     templateUrl: './booking-window.component.html',
     styleUrls: ['./booking-window.component.sass'],
 })
-export class BookingWindowComponent {
+export class BookingWindowComponent extends BaseComponent {
     maxVisibleMembers: number = 10;
-
-    title: string;
-
-    message?: string;
-
-    titleImagePath?: string;
-
-    buttonsOptions: IConfirmButtonOptions[];
-
-    dateTime?: Date;
-
-    duration?: number;
-
-    meetingName?: string;
-
-    participants?: INewMeetingMember[];
-
-    location?: LocationType;
-
-    link?: string;
 
     locationTypeMapping = LocationTypeMapping;
 
@@ -40,16 +18,7 @@ export class BookingWindowComponent {
         @Inject(MAT_DIALOG_DATA) public data: IConfirmDialogData,
         private dialogRef: MatDialogRef<BookingWindowComponent>,
     ) {
-        this.title = data.title;
-        this.message = data.message;
-        this.titleImagePath = data.titleImagePath;
-        this.buttonsOptions = data.buttonsOptions;
-        this.dateTime = data.dateTime;
-        this.duration = data.duration;
-        this.meetingName = data.meetingName;
-        this.participants = data.participants;
-        this.location = data.location;
-        this.link = data.link;
+        super();
     }
 
     onClick(event: EventEmitter<void>) {
