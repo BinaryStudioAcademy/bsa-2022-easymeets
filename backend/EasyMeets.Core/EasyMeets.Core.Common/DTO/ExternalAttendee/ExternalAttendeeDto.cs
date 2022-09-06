ba191e4b31e718ee90b22ea83be8406c7ac202c5
Vasiliy <vasiokpavluyk@gmail.com>
2022-09-06 09:30:41 +0300

    fixed bugs on external booking

--- backend/EasyMeets.Core/EasyMeets.Core.Common/DTO/ExternalAttendee/ExternalAttendeeDto.cs
+++ backend/EasyMeets.Core/EasyMeets.Core.Common/DTO/ExternalAttendee/ExternalAttendeeDto.cs
@@ -1,11 +1,11 @@
-using EasyMeets.Core.Common.DTO.Common;
-
 namespace EasyMeets.Core.Common.DTO.ExternalAttendee;
 
-public class ExternalAttendeeDto : TimeZoneDto
+public class ExternalAttendeeDto
 {
     public long AvailabilitySlotId { get; set; }
     public long MeetingId { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
+    public string TimeZoneName { get; set; } = string.Empty;
+    public string TimeZoneValue { get; set; } = string.Empty;
 }
