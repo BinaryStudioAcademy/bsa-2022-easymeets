f6a5e3e7dc256d576153cde963d95d16522fdaf0
Olena Kozak <olena.s.kozak@gmail.com>
2022-09-13 19:51:12 +0300

    Refactored new meeting member dto

--- backend/EasyMeets.Core/EasyMeets.Core.BLL/MappingProfiles/TeamMemberProfile.cs
+++ backend/EasyMeets.Core/EasyMeets.Core.BLL/MappingProfiles/TeamMemberProfile.cs
@@ -11,7 +11,7 @@ namespace EasyMeets.Core.BLL.MappingProfiles
             CreateMap<TeamMember, NewMeetingMemberDto>()
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.User.Name))
                 .ForMember(dest => dest.Image, opt => opt.MapFrom(src => src.User.ImagePath))
-                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.User.Id));
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id));
             CreateMap<NewMeetingMemberDto, SlotMember>()
                .ForMember(dest => dest.MemberId, opt => opt.MapFrom(src => src.Id)); 
         }
