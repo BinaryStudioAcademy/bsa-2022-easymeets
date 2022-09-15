092aed64928b091fe3ef3135235d30417104e623
Koval Vadym <vzlobinkov@gmail.com>
2022-09-15 20:03:23 +0300

    Add back method exception

--- backend/EasyMeets.Core/EasyMeets.Core.BLL/Services/AvailabilityService.cs
+++ backend/EasyMeets.Core/EasyMeets.Core.BLL/Services/AvailabilityService.cs
@@ -321,7 +321,7 @@ namespace EasyMeets.Core.BLL.Services
 
         public async Task<bool> ValidateSlotPasswordAsync(string slotLink, string password)
         {
-            var slot = await _context.AvailabilitySlots.FirstOrDefaultAsync(el => el.Link == slotLink);
+            var slot = await _context.AvailabilitySlots.FirstOrDefaultAsync(el => el.Link == slotLink) ?? throw new KeyNotFoundException("Availability slot doesn't exist");
             return slot?.PasswordProtection == password;
         }
 
