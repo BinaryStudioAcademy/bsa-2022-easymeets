a66adbf362662e14a84727e21b5aa037a6dbce4d
Koval Vadym <vzlobinkov@gmail.com>
2022-09-20 14:44:43 +0300

    Add team slot

--- frontend/src/app/modules/availability/team/team.component.ts
+++ frontend/src/app/modules/availability/team/team.component.ts
@@ -1,8 +1,11 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { BaseComponent } from '@core/base/base.component';
+import { Color } from '@core/enums/color.enum';
 import { IAvailabilitySlot } from '@core/models/IAvailabilitySlot';
 import { IAvailabilitySlotsGroupByTeamsDto } from '@core/models/IAvailabilitySlotsGroupByTeam';
+import { ISchedule } from '@core/models/schedule/ISchedule';
+import { LocationType } from '@shared/enums/locationType';
 
 @Component({
     selector: 'app-team',
@@ -23,6 +26,37 @@ export class TeamComponent extends BaseComponent implements OnInit {
     }
 
     ngOnInit(): void {
+        this.teamSlot = {
+            id: 100n,
+            name: 'CorgiTeam',
+            image: '',
+            availabilitySlots: [
+                {
+                    id: 103n,
+                    teamId: 63n,
+                    name: 'TeamSlot',
+                    authorName: '',
+                    size: 5,
+                    link: 'link',
+                    isEnabled: true,
+                    isVisible: true,
+                    locationType: LocationType.GoogleMeet,
+                    welcomeMessage: 'welcomeMessage',
+                    color: Color.Cyan,
+                    language: 'language',
+                    bookingsPerDay: 10,
+                    passwordProtectionIsUsed: false,
+                    timeZoneVisibility: false,
+                    questions: [],
+                    schedule: {} as ISchedule,
+                    members: [
+                        { id: 100n, name: 'Member1' },
+                        { id: 101n, name: 'Member2' },
+                        { id: 102n, name: 'Member3' },
+                    ],
+                },
+            ],
+        };
         this.slots = this.teamSlot.availabilitySlots;
         this.teamName = this.teamSlot.name;
     }
