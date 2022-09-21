5c1a57c6d8dc9053bb601d5275b3a6eba6bee33d
Hekeem <79774156+hekeemje@users.noreply.github.com>
2022-09-21 11:50:35 +0300

    check meetings only what we need now

--- backend/EasyMeets.Core/EasyMeets.Core.BLL/Services/Quartz/EmailDelayService.cs
+++ backend/EasyMeets.Core/EasyMeets.Core.BLL/Services/Quartz/EmailDelayService.cs
@@ -2,7 +2,6 @@
 using EasyMeets.Core.BLL.Interfaces;
 using EasyMeets.Core.Common.Enums;
 using EasyMeets.Core.DAL.Context;
-using EasyMeets.Core.DAL.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace EasyMeets.Core.BLL.Services.Quartz
@@ -18,53 +17,15 @@ namespace EasyMeets.Core.BLL.Services.Quartz
 
         public async Task CheckForNotify(TemplateType templateType)
         {
-            var emailTemplates = await _context.EmailTemplates
-                .Where(x => x.TemplateType == templateType && x.IsSend && x.TimeValue != string.Empty)
-                .Include(x => x.AvailabilitySlot)
+            var meetings = await _context.Meetings.Where(x => x.AvailabilitySlot != null && x.AvailabilitySlot.EmailTemplates.Any(x => x.TemplateType == templateType))
+                .Where(meeting => 
+                TimeSpan.Parse(meeting.StartTime.AddMinutes(TimeSpan.Parse(meeting.AvailabilitySlot!.EmailTemplates.FirstOrDefault(x => x.TemplateType == templateType)!.TimeValue).TotalMinutes).ToString("MM/dd/yyyy hh:mm")) 
+                == TimeSpan.Parse(DateTimeOffset.Now.ToString("MM/dd/yyyy hh:mm")))
                 .ToListAsync();
 
-            await Notify(emailTemplates);
-        }
-
-        public async Task Notify(List<EmailTemplate> templates)
-        {
-            foreach (var emailTemplate in templates)
+            if (meetings.Any())
             {
-                var slot = emailTemplate.AvailabilitySlot;
-
-                if (slot.Meetings is null || !slot.Meetings.Any())
-                {
-                    break;
-                }
-
-                double minutes = TimeSpan.Parse(emailTemplate.TimeValue).TotalMinutes;
-
-                if (emailTemplate.TemplateType == TemplateType.Reminders)
-                {
-                    minutes *= -1;
-                }
-
-                await Task.WhenAll(slot.Meetings.Select(x => CheckAndSend(minutes, x, emailTemplate)));
-            }
-        }
-
-        public async Task CheckAndSend(double minutes, Meeting meeting, EmailTemplate emailTemplate)
-        {
-            if (meeting.StartTime.AddMinutes(minutes) == DateTimeOffset.Now)
-            {
-                try
-                {
-                    await _meetingService.SendEmailsAsync(meeting.Id, emailTemplate.TemplateType);
-                }
-                catch (Exception ex)
-                {
-                    throw new Exception($"Error in Quartz worker. Error : {ex.Message}");
-                }
-                finally
-                {
-                    _context.EmailTemplates.Remove(emailTemplate);
-                    await _context.SaveChangesAsync();
-                }
+                await Task.WhenAll(meetings.Select(meeting => _meetingService.SendEmailsAsync(meeting.Id, templateType)));
             }
         }
     }
