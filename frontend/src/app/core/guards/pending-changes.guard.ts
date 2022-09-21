2f9a1c66b94c208243ede0dd15d880865ff8d003
Vasiliy <vasiokpavluyk@gmail.com>
2022-09-22 00:05:35 +0300

    spinner hide fix

--- frontend/src/app/core/guards/pending-changes.guard.ts
+++ frontend/src/app/core/guards/pending-changes.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanDeactivate } from '@angular/router';
 import { ConfirmationWindowService } from '@core/services/confirmation-window.service';
+import { SpinnerService } from '@core/services/spinner.service';
 import { Observable } from 'rxjs';
 
 export interface ComponentCanDeactivate {
@@ -10,9 +11,11 @@ export interface ComponentCanDeactivate {
 @Injectable()
 export class PendingChangesGuard implements CanDeactivate<ComponentCanDeactivate> {
     // eslint-disable-next-line no-empty-function
-    constructor(private confirmWindowService: ConfirmationWindowService) {}
+    constructor(private confirmWindowService: ConfirmationWindowService, private spinner: SpinnerService) {}
 
     canDeactivate(component: ComponentCanDeactivate): boolean | Observable<boolean> {
+        this.spinner.hide();
+
         return component.canDeactivate() ? true : this.confirmWindowService.confirmLeaveDialog();
     }
 }
