4489e0cddc3c890bbbb493166d669866838e4264
Hekeem <79774156+hekeemje@users.noreply.github.com>
2022-09-21 11:57:07 +0300

    add validation for IsSend and TimeValue

--- backend/EasyMeets.Core/EasyMeets.Core.BLL/Services/Quartz/EmailDelayService.cs
+++ backend/EasyMeets.Core/EasyMeets.Core.BLL/Services/Quartz/EmailDelayService.cs
@@ -17,7 +17,7 @@ namespace EasyMeets.Core.BLL.Services.Quartz
 
         public async Task CheckForNotify(TemplateType templateType)
         {
-            var meetings = await _context.Meetings.Where(x => x.AvailabilitySlot != null && x.AvailabilitySlot.EmailTemplates.Any(x => x.TemplateType == templateType))
+            var meetings = await _context.Meetings.Where(x => x.AvailabilitySlot != null && x.AvailabilitySlot.EmailTemplates.Any(x => x.TemplateType == templateType && x.IsSend && x.TimeValue != string.Empty))
                 .Where(meeting => 
                 TimeSpan.Parse(meeting.StartTime.AddMinutes(TimeSpan.Parse(meeting.AvailabilitySlot!.EmailTemplates.FirstOrDefault(x => x.TemplateType == templateType)!.TimeValue).TotalMinutes).ToString("MM/dd/yyyy hh:mm")) 
                 == TimeSpan.Parse(DateTimeOffset.Now.ToString("MM/dd/yyyy hh:mm")))
