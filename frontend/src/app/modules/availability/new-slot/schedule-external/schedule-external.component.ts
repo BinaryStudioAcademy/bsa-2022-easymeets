0a2e42e0f375d27219e4e96cc7e3f549901ded2c
Koval Vadym <vzlobinkov@gmail.com>
2022-09-21 22:28:06 +0300

    Change styles/Add fields

--- frontend/src/app/modules/availability/new-slot/schedule-external/schedule-external.component.ts
+++ frontend/src/app/modules/availability/new-slot/schedule-external/schedule-external.component.ts
@@ -1,42 +1,115 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BaseComponent } from '@core/base/base.component';
 import { ExternalScheduleState } from '@core/enums/external-schedule-state.enum';
 import { getDefaultSchedule } from '@core/helpers/default-schedule-helper';
+import { getDisplayDuration, getInputSettings } from '@core/helpers/display-duration-helper';
+import { LocationTypeMapping } from '@core/helpers/location-type-mapping';
 import { getScheduleItems } from '@core/helpers/schedule-list-helper';
+import { removeExcessiveSpaces } from '@core/helpers/string-helper';
 import { IAvailabilitySlot } from '@core/models/IAvailabilitySlot';
+import { IDuration } from '@core/models/IDuration';
+import { IInputSettings } from '@core/models/save-availability-slot/IInputSettings';
 import { ISchedule } from '@core/models/schedule/ISchedule';
 import { AvailabilitySlotService } from '@core/services/availability-slot.service';
 import { NotificationService } from '@core/services/notification.service';
-import { emailRegex } from '@shared/constants/model-validation';
+import { UserService } from '@core/services/user.service';
+import { emailRegex, textFieldRegex } from '@shared/constants/model-validation';
+import { invalidCharactersMessage } from '@shared/constants/shared-messages';
+import { GeneralKeys, InputFieldType } from '@shared/enums/custom-field';
+import { LocationType } from '@shared/enums/locationType';
 
 @Component({
     selector: 'app-schedule-external',
     templateUrl: './schedule-external.component.html',
     styleUrls: ['./schedule-external.component.sass'],
 })
-export class ScheduleExternalComponent extends BaseComponent {
-    public state: ExternalScheduleState = ExternalScheduleState.ScheduleDefining;
+export class ScheduleExternalComponent extends BaseComponent implements OnInit {
+    state: ExternalScheduleState = ExternalScheduleState.ScheduleDefining;
 
-    public link: string;
+    link: string;
 
-    public slot?: IAvailabilitySlot;
+    slot?: IAvailabilitySlot;
 
-    public schedule: ISchedule = getDefaultSchedule(true);
+    schedule: ISchedule = getDefaultSchedule(true);
 
-    public emailPattern = emailRegex;
+    emailPattern = emailRegex;
+
+    textFieldRegex = textFieldRegex;
+
+    invalidCharactersMessage = invalidCharactersMessage;
+
+    locations: LocationType[];
+
+    durations: IDuration[] = getDisplayDuration();
+
+    durationKey = { inputType: InputFieldType.Duration, field: GeneralKeys.size };
+
+    inputSettings: { [type: string]: IInputSettings } = getInputSettings();
+
+    inputValues: { [type: string]: number } = {};
+
+    keys = Object.keys(InputFieldType);
+
+    locationTypeMapping = LocationTypeMapping;
+
+    LocationType = LocationType;
 
     constructor(
         private router: Router,
         private activatedRoute: ActivatedRoute,
         private notificationsService: NotificationService,
         private slotService: AvailabilitySlotService,
+        private userService: UserService,
     ) {
         super();
         activatedRoute.params.subscribe((params) => {
             this.link = params['link'];
             this.getSlotFromLink();
         });
+    }
+
+    ngOnInit(): void {
+        this.initLocations();
+        this.defineDuration();
+    }
+
+    nextClicked() {
+        this.state = ExternalScheduleState.EmailDefining;
+    }
+
+    doneClicked() {
+        this.slotService
+            .updateScheduleExternally(this.link, this.schedule)
+            .pipe(this.untilThis)
+            .subscribe(
+                () => {
+                    this.state = ExternalScheduleState.Done;
+                },
+                () => {
+                    this.notificationsService.showErrorMessage('Something went wrong, try defining schedule again');
+                    this.returnToSchedule();
+                },
+            );
+    }
+
+    returnToSchedule() {
+        this.state = ExternalScheduleState.ScheduleDefining;
+    }
+
+    meetingNameChanged(value: string) {
+        if (this.slot) {
+            this.slot.name = removeExcessiveSpaces(value);
+        }
+    }
+
+    private initLocations() {
+        this.userService
+            .getUserMeetIntegrations()
+            .pipe(this.untilThis)
+            .subscribe((locations) => {
+                this.locations = locations.concat(LocationType.Office);
+            });
     }
 
     private getSlotFromLink() {
@@ -58,26 +131,30 @@ export class ScheduleExternalComponent extends BaseComponent {
             });
     }
 
-    public nextClicked() {
-        this.state = ExternalScheduleState.EmailDefining;
+    private defineDuration() {
+        this.keys.forEach((el) => {
+            const duration =
+                this.durations.find((x) => x.minutes === this.inputValues[this.inputSettings[el].inputType]) ??
+                this.durations.find((x) => x.time === 'Custom');
+
+            this.inputSettings[el] = {
+                ...this.inputSettings[el],
+                durationValue: duration,
+            };
+
+            if (duration?.time === 'Custom') {
+                this.inputSettings[el] = {
+                    ...this.inputSettings[el],
+                    isCustom: true,
+                    inputValue: String(this.inputValues[this.inputSettings[el].inputType]),
+                };
+            }
+        });
     }
 
-    public doneClicked() {
-        this.slotService
-            .updateScheduleExternally(this.link, this.schedule)
-            .pipe(this.untilThis)
-            .subscribe(
-                () => {
-                    this.state = ExternalScheduleState.Done;
-                },
-                () => {
-                    this.notificationsService.showErrorMessage('Something went wrong, try defining schedule again');
-                    this.returnToSchedule();
-                },
-            );
-    }
-
-    public returnToSchedule() {
-        this.state = ExternalScheduleState.ScheduleDefining;
+    private defineCurrentInputValues() {
+        if (this.slot) {
+            this.inputValues[InputFieldType.Duration] = this.slot.size;
+        }
     }
 }
