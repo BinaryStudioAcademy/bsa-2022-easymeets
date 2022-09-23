263b4b54f45b22961ef5ff7155ea3cc622c31a2d
Mykhailo Huzhvii <huzhvii.m@gmail.com>
2022-09-23 21:15:01 +0300

    Fix switch on esc

--- frontend/src/app/shared/components/confirmation-window/confirmation-window.component.ts
+++ frontend/src/app/shared/components/confirmation-window/confirmation-window.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Inject } from '@angular/core';
+import { Component, EventEmitter, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { BaseComponent } from '@core/base/base.component';
 import { LocationTypeMapping } from '@core/helpers/location-type-mapping';
 import { IConfirmButtonOptions } from '@shared/models/confirmWindow/IConfirmButtonOptions';
 import { IConfirmDialogData } from '@shared/models/confirmWindow/IConfirmDialogData';
@@ -9,7 +10,7 @@ import { IConfirmDialogData } from '@shared/models/confirmWindow/IConfirmDialogD
     templateUrl: './confirmation-window.component.html',
     styleUrls: ['./confirmation-window.component.sass'],
 })
-export class ConfirmationWindowComponent {
+export class ConfirmationWindowComponent extends BaseComponent implements OnInit {
     title: string;
 
     message?: string;
@@ -22,9 +23,21 @@ export class ConfirmationWindowComponent {
         @Inject(MAT_DIALOG_DATA) public data: IConfirmDialogData,
         private dialogRef: MatDialogRef<ConfirmationWindowComponent>,
     ) {
+        super();
         this.title = data.title;
         this.message = data.message;
         this.buttonsOptions = data.buttonsOptions;
+    }
+
+    ngOnInit(): void {
+        this.dialogRef
+            .keydownEvents()
+            .pipe(this.untilThis)
+            .subscribe((event) => {
+                if (event.key === 'Escape') {
+                    this.dialogRef.close();
+                }
+            });
     }
 
     onClick(event: EventEmitter<void>) {
